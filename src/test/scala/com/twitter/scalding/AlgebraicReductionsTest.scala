a19116978f8d2f319014077e04edacbd1bd7bc2b
Argyris Zymnis <argyris@twitter.com>
2012-04-17 21:55:43 -0700

    Break up autogenerated and manually written code in AbstractAlgebra.

--- src/test/scala/com/twitter/scalding/AlgebraicReductionsTest.scala
+++ src/test/scala/com/twitter/scalding/AlgebraicReductionsTest.scala
@@ -15,7 +15,17 @@ class AlgebraJob(args : Args) extends Job(args) {
     .write(Tsv("output"))
 }
 
+class ComplicatedAlgebraJob(args : Args) extends Job(args) {
+  Tsv("input", ('x,'y,'z,'w,'v))
+    .map('w -> 'w) { w : Int => Set(w) }
+    .groupBy('x) {
+      _.plus[(Int,Int,Set[Int],Double)](('y,'z,'w,'v) -> ('sy,'sz,'sw,'sv))
+    }
+    .write(Tsv("output"))
+}
+
 class AlgebraJobTest extends Specification {
+  noDetailedDiffs()
   import RichPipe._
   val inputData = List((1,2,3,5),(1,4,5,7),(2,1,0,7))
   val correctOutput = List((1,6,8,Set(5,7), 8,15,(6 + 20)),(2,1,0,Set(7),1,0,0))
@@ -25,6 +35,20 @@ class AlgebraJobTest extends Specification {
       .sink[(Int, Int, Int, Set[Int], Int, Int, Int)](Tsv("output")) { buf =>
         "correctly do algebra" in {
           buf.toList must be_==(correctOutput)
+        }
+      }
+      .run
+      .finish
+  }
+
+  val inputData2 = List((1,2,3,5,1.2),(1,4,5,7,0.1),(2,1,0,7,3.2))
+  val correctOutput2 = List((1,6,8,Set(5,7),1.3),(2,1,0,Set(7),3.2))
+  "A ComplicatedAlgebraJob" should {
+    JobTest("com.twitter.scalding.ComplicatedAlgebraJob")
+      .source(Tsv("input",('x,'y,'z,'w,'v)), inputData2)
+      .sink[(Int, Int, Int, Set[Int], Double)](Tsv("output")) { buf =>
+        "correctly do complex algebra" in {
+          buf.toList must be_==(correctOutput2)
         }
       }
       .run
