999c377abbb6f35a4550f780c7ed179e66aabf0e
Oscar Boykin <oscar@twitter.com>
2012-07-17 23:46:33 -0700

    Remove ClassMap as it didn't solve the issue it was intended (and likely less efficient)

--- src/main/scala/com/twitter/scalding/serialization/KryoSerializers.scala
+++ src/main/scala/com/twitter/scalding/serialization/KryoSerializers.scala
@@ -36,66 +36,6 @@ import scala.collection.immutable.ListMap
 import com.twitter.scalding.DateRange
 import com.twitter.scalding.RichDate
 
-object ClassMap {
-  implicit def apply(objects : Iterable[_]) = {
-    new ClassMap(objects
-      .map { _.asInstanceOf[AnyRef].getClass }
-      .toSet //Make sure they are all unique
-      .toIndexedSeq)
-  }
-  def read(in : Input) : ClassMap = {
-    val size = in.readInt(true);
-    val classSeq = (0 until size).foldLeft(Vector[Class[_]]()) { (cls, idx) =>
-      cls :+ Class.forName(in.readString)
-    }
-    new ClassMap(classSeq)
-  }
-}
-
-/**
- * We've been seeing some issues with letting Kryo track the classes.
- * This is arguably a hack, and Kryo should handle it, but we're working
- * around for now.
- */
-class ClassMap(val idxToClass : IndexedSeq[Class[_]]) {
-  // Get the index of a class
-  lazy val idxOf = idxToClass.zipWithIndex.toMap
-  def apply(cls : Class[_]) : Int = idxOf(cls)
-  def apply(idx : Int) = idxToClass(idx)
-  def :+(klass : Class[_]) : ClassMap = {
-    if (idxOf.contains(klass)) {
-      this
-    }
-    else {
-      val newIdxToClass = idxToClass :+ klass
-      new ClassMap(newIdxToClass)
-    }
-  }
-  def ++(other : ClassMap) : ClassMap = {
-    if(idxToClass.size < other.idxToClass.size) {
-      //Reverse:
-      other ++ this
-    }
-    else {
-      other.idxToClass.foldLeft(this) { (cm, cls) => cm :+ cls }
-    }
-  }
-
-  def write(out : Output) {
-    out.writeInt(idxToClass.size, true)
-    idxToClass.foreach { cls => out.writeString(cls.getName) }
-  }
-  def writeClass(out : Output, klass : Class[_]) {
-    out.writeInt(apply(klass), true)
-  }
-  def readClass(in : Input) : Class[_] = {
-    apply(in.readInt(true))
-  }
-  override def toString = {
-    "ClassMap(" + idxToClass.mkString(", ") + ")"
-  }
-}
-
 // Singletons are easy, you just return the singleton and don't read:
 // It's important you actually do this, or Kryo will generate Nil != Nil, or None != None
 class SingletonSerializer[T](obj: T) extends KSerializer[T] {
@@ -106,9 +46,6 @@ class SingletonSerializer[T](obj: T) extends KSerializer[T] {
 // Long Lists cause stack overflows for Kryo because they are cons cells.
 class ListSerializer[V, T <: List[V]](emptyList : List[V]) extends KSerializer[T] {
   def write(kser: Kryo, out: Output, obj: T) {
-    // Kryo is not efficient with deeper references, so we handle it manually:
-    val cm = ClassMap(obj)
-    cm.write(out)
     //Write the size:
     out.writeInt(obj.size, true)
     /*
@@ -122,30 +59,21 @@ class ListSerializer[V, T <: List[V]](emptyList : List[V]) extends KSerializer[T
      */
     obj.foreach { t =>
       val tRef = t.asInstanceOf[AnyRef]
-      cm.writeClass(out, tRef.getClass)
-      kser.writeObject(out, tRef)
+      kser.writeClassAndObject(out, tRef)
       // After each itermediate object, flush
       out.flush
     }
   }
 
   def read(kser: Kryo, in: Input, cls: Class[T]) : T = {
-    //Read the classmap:
-    val cm = ClassMap.read(in)
-    val size = in.readInt(true);
-
     //Produce the reversed list:
+    val size = in.readInt(true)
     if (size == 0) {
-      /*
-       * this is only here at compile time.  The type T is erased, but the
-       * compiler verifies that we are intending to return a type T here.
-       */
       emptyList.asInstanceOf[T]
     }
     else {
       (0 until size).foldLeft(emptyList) { (l, i) =>
-        val klass = cm.readClass(in).asInstanceOf[Class[V]]
-        val iT = kser.readObject(in, klass)
+        val iT = kser.readClassAndObject(in).asInstanceOf[V]
         iT :: l
       }.reverse.asInstanceOf[T]
     }
@@ -154,26 +82,19 @@ class ListSerializer[V, T <: List[V]](emptyList : List[V]) extends KSerializer[T
 
 class VectorSerializer[T] extends KSerializer[Vector[T]] {
   def write(kser: Kryo, out: Output, obj: Vector[T]) {
-    //Write the classMap
-    val cm = ClassMap(obj)
-    cm.write(out)
     //Write the size:
     out.writeInt(obj.size, true)
     obj.foreach { t  =>
       val tRef = t.asInstanceOf[AnyRef]
-      cm.writeClass(out, tRef.getClass)
-      kser.writeObject(out, tRef)
+      kser.writeClassAndObject(out, tRef)
       // After each intermediate object, flush
       out.flush
     }
   }
 
   def read(kser: Kryo, in: Input, cls: Class[Vector[T]]) : Vector[T] = {
-    //Read the classmap:
-    val cm = ClassMap.read(in)
-    val size = in.readInt(true);
-
     //Produce the reversed list:
+    val size = in.readInt(true)
     if (size == 0) {
       /*
        * this is only here at compile time.  The type T is erased, but the
@@ -183,8 +104,7 @@ class VectorSerializer[T] extends KSerializer[Vector[T]] {
     }
     else {
       (0 until size).foldLeft(Vector.empty[T]) { (vec, i) =>
-        val klass = cm.readClass(in).asInstanceOf[Class[T]]
-        val iT = kser.readObject(in, klass)
+        val iT = kser.readClassAndObject(in).asInstanceOf[T]
         vec :+ iT
       }
     }
@@ -194,41 +114,27 @@ class VectorSerializer[T] extends KSerializer[Vector[T]] {
 
 class MapSerializer[K,V,T <: Map[K,V]](emptyMap : Map[K,V]) extends KSerializer[T] {
   def write(kser: Kryo, out: Output, obj: T) {
-    //Write the size:
-    val cm = ClassMap(obj.keys ++ obj.values)
-    cm.write(out)
-
     out.writeInt(obj.size, true)
     obj.foreach { pair : (K,V) =>
       val kRef = pair._1.asInstanceOf[AnyRef]
-      val vRef = pair._2.asInstanceOf[AnyRef]
-      cm.writeClass(out, kRef.getClass)
-      kser.writeObject(out, kRef)
+      kser.writeClassAndObject(out, kRef)
       out.flush
-      cm.writeClass(out, vRef.getClass)
-      kser.writeObject(out, vRef)
+
+      val vRef = pair._2.asInstanceOf[AnyRef]
+      kser.writeClassAndObject(out, vRef)
       out.flush
     }
   }
 
   def read(kser: Kryo, in: Input, cls: Class[T]) : T = {
-    val cm = ClassMap.read(in)
-    val size = in.readInt(true);
-
+    val size = in.readInt(true)
     if (size == 0) {
-      /*
-       * this cast is only here at compile time.  The type T is erased, but the
-       * compiler verifies that we are intending to return a type T here.
-       */
       emptyMap.asInstanceOf[T]
     }
     else {
       (0 until size).foldLeft(emptyMap) { (map, i) =>
-        val kklass = cm.readClass(in).asInstanceOf[Class[K]]
-        val key = kser.readObject(in, kklass)
-
-        val vklass = cm.readClass(in).asInstanceOf[Class[V]]
-        val value = kser.readObject(in, vklass)
+        val key = kser.readClassAndObject(in).asInstanceOf[K]
+        val value = kser.readClassAndObject(in).asInstanceOf[V]
 
         map + (key -> value)
       }.asInstanceOf[T]
