d96c0081d75b6b7e68e0ab12200c3f096cf54c3d
Sam Ritchie <sritchie@twitter.com>
2013-02-15 12:13:11 -0800

    updated site

--- project/Build.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-
-object ScaldingBuild extends Build {
-  lazy val root = Project("root", file("."))
-// This caused us some pain:
-//                    .dependsOn(RootProject(uri("git://github.com/twitter/algebird.git#master")))
-//                    .dependsOn(RootProject(uri("git://github.com/twitter/chill.git#master")))
-}
--- tutorial/CodeSnippets.md
+++
@@ -1,1 +0,0 @@
-Please see the [API reference](https://github.com/twitter/scalding/wiki/API-Reference) on the wiki.
