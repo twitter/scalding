b6656a27ef9d51cb4296480e54877d3b70866f4f
Thomas Schroeter <thomas@cliqz.com>
2013-03-14 23:59:31 +0100

    run tutorial in travis test
    add matrix tutorials to test script

--- scripts/test_tutorials.sh
+++ scripts/test_tutorials.sh
@@ -1,4 +1,3 @@
-sbt assembly
 scripts/scald.rb --local tutorial/Tutorial0.scala
 scripts/scald.rb --local tutorial/Tutorial1.scala
 scripts/scald.rb --local tutorial/Tutorial2.scala
@@ -13,3 +12,27 @@ scripts/scald.rb \
   --local tutorial/Tutorial5.scala \
   --input tutorial/data/hello.txt \
   --output tutorial/data/output5.txt
+scripts/scald.rb \
+  --local tutorial/MatrixTutorial0.scala \
+  --input tutorial/data/graph.tsv \
+  --output tutorial/data/outdegree.tsv
+scripts/scald.rb \
+  --local tutorial/MatrixTutorial1.scala \
+  --input tutorial/data/graph.tsv \
+  --output tutorial/data/cofollows.tsv
+scripts/scald.rb \
+  --local tutorial/MatrixTutorial2.scala \
+  --input tutorial/data/graph.tsv \
+  --maxOutdegree 1000 \
+  --output data/graphFiltered.tsv
+scripts/scald.rb \
+  --local tutorial/MatrixTutorial3.scala \
+  --input1 tutorial/data/graph.tsv \
+  --input2 tutorial/data/graph2.tsv \
+  --intersection tutorial/data/intersection.tsv \
+  --leftDiff tutorial/data/leftDiff.tsv \
+  --rightDiff tutorial/data/rightDiff.tsv
+scripts/scald.rb \
+  --local tutorial/MatrixTutorial5.scala \
+  --input tutorial/data/graph.tsv \
+  --output tutorial/data/cosineSim.tsv
