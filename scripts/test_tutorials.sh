673b87d0cab1756e3c20dd2b129af9d5f47342bd
Adam Pingel <pingel@gmail.com>
2013-03-21 17:15:02 -0700

    use set -e

--- scripts/test_tutorials.sh
+++ scripts/test_tutorials.sh
@@ -1,47 +1,53 @@
-scripts/scald.rb --local tutorial/Tutorial0.scala || exit 1
-scripts/scald.rb --local tutorial/Tutorial1.scala || exit 1
-scripts/scald.rb --local tutorial/Tutorial2.scala || exit 1
-scripts/scald.rb \
+
+set -e # first error should stop execution of this script
+
+SCALD="scripts/scald.rb"
+
+$SCALD --local tutorial/Tutorial0.scala
+$SCALD --local tutorial/Tutorial1.scala
+$SCALD --local tutorial/Tutorial2.scala
+
+$SCALD \
   --local tutorial/Tutorial3.scala \
-  --input tutorial/data/hello.txt \
-  || exit 1
-scripts/scald.rb \
+  --input tutorial/data/hello.txt
+ 
+$SCALD \
   --local tutorial/Tutorial4.scala \
   --input tutorial/data/hello.txt \
-  --output tutorial/data/output4.txt \
-  || exit 1
-scripts/scald.rb \
+  --output tutorial/data/output4.txt
+ 
+$SCALD \
   --local tutorial/Tutorial5.scala \
   --input tutorial/data/hello.txt \
   --output tutorial/data/output5.txt \
-  --words tutorial/data/words.txt \
-  || exit 1
-scripts/scald.rb \
+  --words tutorial/data/words.txt
+ 
+$SCALD \
   --local tutorial/MatrixTutorial0.scala \
   --input tutorial/data/graph.tsv \
-  --output tutorial/data/outdegree.tsv \
-  || exit 1
-scripts/scald.rb \
+  --output tutorial/data/outdegree.tsv
+ 
+$SCALD \
   --local tutorial/MatrixTutorial1.scala \
   --input tutorial/data/graph.tsv \
-  --output tutorial/data/cofollows.tsv \
-  || exit 1
-scripts/scald.rb \
+  --output tutorial/data/cofollows.tsv
+ 
+$SCALD \
   --local tutorial/MatrixTutorial2.scala \
   --input tutorial/data/graph.tsv \
   --maxOutdegree 1000 \
-  --output tutorial/data/graphFiltered.tsv \
-  || exit 1
-scripts/scald.rb \
+  --output tutorial/data/graphFiltered.tsv
+ 
+$SCALD \
   --local tutorial/MatrixTutorial3.scala \
   --input1 tutorial/data/graph.tsv \
   --input2 tutorial/data/graph2.tsv \
   --intersection tutorial/data/intersection.tsv \
   --leftDiff tutorial/data/leftDiff.tsv \
-  --rightDiff tutorial/data/rightDiff.tsv \
-  || exit 1
-scripts/scald.rb \
+  --rightDiff tutorial/data/rightDiff.tsv
+ 
+$SCALD \
   --local tutorial/MatrixTutorial5.scala \
   --input tutorial/data/graph.tsv \
-  --output tutorial/data/cosineSim.tsv \
-  || exit 1
+  --output tutorial/data/cosineSim.tsv
+ 
