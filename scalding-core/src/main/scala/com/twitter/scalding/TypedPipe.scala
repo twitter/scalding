5b91d13add76fe0d239d53e9328f5ce7fb541958
David Shimon <david206@gmail.com>
2013-04-15 00:54:16 +0300

    use .pipe on TypedPipe.limit instead of .innerPipe

--- scalding-core/src/main/scala/com/twitter/scalding/TypedPipe.scala
+++ scalding-core/src/main/scala/com/twitter/scalding/TypedPipe.scala
@@ -106,7 +106,7 @@ class TypedPipe[+T] private (inpipe : Pipe, fields : Fields, flatMapFn : (TupleE
    * The number may be less than count, and not sampled particular method
    */
   def limit(count: Int): TypedPipe[T] =
-    TypedPipe.from[T](inpipe.limit(count), 0)
+    TypedPipe.from[T](pipe.limit(count), 0)
 
   def map[U](f : T => U) : TypedPipe[U] = {
     new TypedPipe[U](inpipe, fields, { te => flatMapFn(te).map(f) })
