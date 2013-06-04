9ce8060d8251b54767cbf5f73b0daba243b4230d
Kevin Lin <klin@twitter.com>
2013-06-03 21:21:06 -0700

    Change timeout parameter in HangingTest to 3 milliseconds, since currently when this flaky test fails it always has more outputs than expected.

--- scalding-core/src/test/scala/com/twitter/scalding/CoreTest.scala
+++ scalding-core/src/test/scala/com/twitter/scalding/CoreTest.scala
@@ -1469,7 +1469,7 @@ class HangingJob(args : Args) extends Job(args) {
     .read
     .filter('x, 'y) { t: (Int, Int) =>
       val (x, y) = t
-      timeout(Millisecs(10)) {
+      timeout(Millisecs(5)) {
         if (y % 2 == 1) Thread.sleep(1000)
         x > 0
       } getOrElse false
