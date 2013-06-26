ea710c3997c1db3aa926687ce7408c2d72e7a1a1
Tomas Tauber <s0943263@sms.ed.ac.uk>
2013-06-26 22:46:10 +0200

    Non-polymorphic and naive sum (as in the mockup) for Scalding

--- scalding-core/src/main/scala/com/twitter/scalding/mathematics/Matrix2.scala
+++ scalding-core/src/main/scala/com/twitter/scalding/mathematics/Matrix2.scala
@@ -4,26 +4,45 @@ import cascading.pipe.Pipe
 import cascading.tuple.Fields
 import com.twitter.scalding.TDsl._
 import com.twitter.scalding._
+import com.twitter.algebird.Monoid
 
-object Matrix2 {
-	sealed abstract class Matrix2[RowT, ColT, ValT]
+object Matrix2 {  
+	sealed abstract class Matrix2
 	  (val sizeHint : SizeHint = NoClue) {
-	  def +(that: Matrix2[RowT, ColT, ValT]):  Matrix2[RowT, ColT, ValT] = Sum(this, that)
-	  def *(that:  Matrix2[RowT, ColT, ValT]):  Matrix2[RowT, ColT, ValT] = Product(this, that)
-	  val tpipe: TypedPipe[(RowT, ColT, ValT)]
+	  def +(that: Matrix2): Matrix2 = Sum(this, that)
+	  def *(that:  Matrix2):  Matrix2 = Product(this, that)
+	  val tpipe: TypedPipe[(Int, Int, Double)]
+  
 	}
 
-	case class Product[RowT, ColT, ValT](val left :  Matrix2[RowT, ColT, ValT], val right :  Matrix2[RowT, ColT, ValT]) extends Matrix2[RowT, ColT, ValT] {
+	case class Product(val left: Matrix2, val right: Matrix2) extends Matrix2 {
 	  override lazy val tpipe = left.tpipe
 	  override val sizeHint = left.sizeHint * right.sizeHint
 	}
 
-	case class Sum[RowT, ColT, ValT](val left:  Matrix2[RowT, ColT, ValT], val right: Matrix2[RowT, ColT, ValT]) extends Matrix2[RowT, ColT, ValT] {
-	  override lazy val tpipe = left.tpipe
+	case class Sum(val left: Matrix2, val right: Matrix2) extends Matrix2 {
+	  def toPipe()(implicit mon : Monoid[Double], ord: Ordering[(Int, Int)]) : TypedPipe[(Int,Int,Double)] = {
+	    if (left.equals(right)) {
+	      left.tpipe.map(v => (v._1, v._2, mon.plus(v._3, v._3)))
+	    } else {
+	      (left.tpipe ++ right.tpipe).groupBy(x => (x._1, x._2)).mapValueStream(vals => {
+	        if (vals.size == 1) vals else {
+	          val l = vals.next()
+	          val r = vals.next()
+	          val res = mon.plus(l._3, r._3)
+	          if (res == mon.zero) Iterator() else Iterator((l._1, l._2, res))
+	        }
+	      }).toTypedPipe.map(y => y._2)
+	      }
+	    }
+	  
+	  override lazy val tpipe = toPipe()
 	  override val sizeHint = left.sizeHint + right.sizeHint
 	}
 
-	case class Literal[RowT, ColT, ValT](val fields: Fields, val inPipe: Pipe, override val sizeHint : SizeHint) extends Matrix2[RowT, ColT, ValT] {
-	  override lazy val tpipe = inPipe.toTypedPipe[(RowT, ColT, ValT)](fields)
+	case class Literal(val fields: Fields, val inPipe: Pipe, override val sizeHint : SizeHint) extends Matrix2 {
+	  override lazy val tpipe = inPipe.toTypedPipe[(Int, Int, Double)](fields)
 	}
+	
+	
 }
--- scalding-core/src/test/scala/com/twitter/scalding/mathematics/Matrix2Test.scala
+++ scalding-core/src/test/scala/com/twitter/scalding/mathematics/Matrix2Test.scala
@@ -5,12 +5,27 @@ import cascading.pipe.joiner._
 import org.specs._
 import com.twitter.algebird.Group
 
-class Matrix2Sum3(args : Args) extends Job(args) {
+
+class Matrix2Sum(args : Args) extends Job(args) {
 
   import Matrix2._
 
   val p1 = Tsv("mat1",('x1,'y1,'v1)).read
-  val mat1 = new Literal[Int,Int,(Double, Double, Double)](('x1,'y1,'v1), p1, NoClue)
+  val mat1 = new Literal(('x1,'y1,'v1), p1, NoClue)
+
+  val p2 = Tsv("mat2",('x2,'y2,'v2)).read
+  val mat2 = new Literal(('x2,'y2,'v2), p2, NoClue)  
+  
+  val sum = mat1 + mat2
+  sum.tpipe.toPipe(('x1,'y1,'v1)).write(Tsv("sum"))
+}
+
+class Matrix2SumSame(args : Args) extends Job(args) {
+
+  import Matrix2._
+
+  val p1 = Tsv("mat1",('x1,'y1,'v1)).read
+  val mat1 = new Literal(('x1,'y1,'v1), p1, NoClue)
 
   val sum = mat1 + mat1
   sum.tpipe.toPipe(('x1,'y1,'v1)).write(Tsv("sum"))
@@ -27,14 +42,30 @@ class Matrix2Test extends Specification {
     iter.map { it => ((it._1, it._1), it._2) }.toMap
   }
 
-  "A Matrix2Sum job, where the Matrix contains tuples as values," should {
+  "A MatrixSum job" should {
     TUtil.printStack {
-    JobTest("com.twitter.scalding.mathematics.Matrix2Sum3")
-      .source(Tsv("mat1",('x1,'y1,'v1)), List((1,1,(1.0, 3.0, 5.0)),(2,2,(3.0, 2.0, 1.0)),(1,2,(4.0, 5.0, 2.0))))
+    JobTest("com.twitter.scalding.mathematics.MatrixSum")
+      .source(Tsv("mat1",('x1,'y1,'v1)), List((1,1,1.0),(2,2,3.0),(1,2,4.0)))
+      .source(Tsv("mat2",('x2,'y2,'v2)), List((1,3,3.0),(2,1,8.0),(1,2,4.0)))
+      .sink[(Int,Int,Double)](Tsv("sum")) { ob =>
+        "correctly compute sums" in {
+          val pMap = toSparseMat(ob)
+          pMap must be_==( Map((1,1)->1.0, (1,2)->8.0, (1,3)->3.0, (2,1)->8.0, (2,2)->3.0))
+        }
+      }
+      .run
+      .finish
+    }
+  }  
+  
+  "A Matrix2SumSame job" should {
+    TUtil.printStack {
+    JobTest("com.twitter.scalding.mathematics.Matrix2SumSame")
+      .source(Tsv("mat1",('x1,'y1,'v1)), List((1,1,1.0),(2,2,3.0),(1,2,4.0)))
       .sink[(Int,Int,(Double, Double, Double))](Tsv("sum")) { ob =>
         "correctly compute sums" in {
           val pMap = toSparseMat(ob)
-          pMap must be_==( Map((1,1)->(2.0, 6.0, 10.0), (2,2)->(6.0, 4.0, 2.0), (1,2)->(8.0, 10.0, 4.0)))
+          pMap must be_==( Map((1,1)->2.0, (2,2)->6.0, (1,2)->8.0))
         }
       }
       .run
