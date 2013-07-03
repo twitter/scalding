f651254c3e1cb8e368c1c410591c231b00f59881
Antonios Chalkiopoulos <Antwnis@gmail.com>
2013-07-03 12:02:21 +0100

    Maybe the order of classes results to the URLClassLoader expeption?

--- scalding-core/src/test/scala/com/twitter/scalding/ScanLeftTest.scala
+++ scalding-core/src/test/scala/com/twitter/scalding/ScanLeftTest.scala
@@ -3,6 +3,65 @@ package com.twitter.scalding
 import org.specs._
 import com.twitter.scalding._
 
+/**
+ * Simple Example: First group data by gender and then sort by height reverse order.
+ * Then add another column for each group which is the rank order of the height.
+ */
+class AddRankingWithScanLeft(args: Args) extends Job(args) {
+
+  TypedTsv[(String, Double)]("input1", ('gender, 'height))
+    .read
+    .groupBy('gender) { group =>
+      group.sortBy('height).reverse
+      group.scanLeft(('height) -> ('rank))((0L)) {
+        (rank: Long, user_id: Double) =>
+          {
+            (rank + 1L)
+          }
+      }
+    }
+    // scanLeft generates an extra line per group, thus remove it 
+    .filter('height) { x: String => x != null }
+    .debug
+    .write(Tsv("result1"))
+
+}
+
+/**
+ * Advanced example: Count seconds each user spent reading a blog article (using scanLeft)
+ * For the sake of simplicity we assume that you have converted date-time into epoch
+ */
+class ScanLeftTimeExample(args: Args) extends Job(args) {
+
+  TypedTsv[(Long, String, String)]("input2", ('epoch, 'user, 'event))
+    // Create a helper symbol first 
+    .insert('temp, 0L)
+    // Group by user and sort by epoch in reverse, so that most recent event comes first
+    .groupBy('user) { group =>
+      group.sortBy('epoch).reverse
+        .scanLeft(('epoch, 'temp) -> ('originalEpoch, 'duration))((0L, 0L)) {
+          (firstLine: (Long, Long), secondLine: (Long, Long)) =>
+            var delta = firstLine._1 - secondLine._1
+            // scanLeft is initialised with (0L,0L) so first subtraction 
+            // will result into a negative number! 
+            if (delta < 0L) delta = -delta
+            (secondLine._1, delta)
+        }
+    }
+    // Remove lines introduced by scanLeft and discard helping symbols
+    .filter('epoch) { x: Any => x != null }
+    .discard('temp, 'originalEpoch)
+    // Order in ascending time
+    .groupBy('user) { group =>
+      group.sortBy('epoch)
+    }
+    // You can now remove most recent events where we are uncertain of time spent
+    .filter('duration) { x: Long => x < 10000L }
+    .debug
+    .write(Tsv("result2"))
+
+}
+
 class ScanLeftTest extends Specification {
   import Dsl._
 
@@ -68,65 +127,6 @@ class ScanLeftTest extends Specification {
       .run
       .finish
   }
-
-}
-
-/**
- * Simple Example: First group data by gender and then sort by height reverse order.
- * Then add another column for each group which is the rank order of the height.
- */
-class AddRankingWithScanLeft(args: Args) extends Job(args) {
-
-  TypedTsv[(String, Double)]("input1", ('gender, 'height))
-    .read
-    .groupBy('gender) { group =>
-      group.sortBy('height).reverse
-      group.scanLeft(('height) -> ('rank))((0L)) {
-        (rank: Long, user_id: Double) =>
-          {
-            (rank + 1L)
-          }
-      }
-    }
-    // scanLeft generates an extra line per group, thus remove it 
-    .filter('height) { x: String => x != null }
-    .debug
-    .write(Tsv("result1"))
-
-}
-
-/**
- * Advanced example: Count seconds each user spent reading a blog article (using scanLeft)
- * For the sake of simplicity we assume that you have converted date-time into epoch
- */
-class ScanLeftTimeExample(args: Args) extends Job(args) {
-
-  TypedTsv[(Long, String, String)]("input2", ('epoch, 'user, 'event))
-    // Create a helper symbol first 
-    .insert('temp, 0L)
-    // Group by user and sort by epoch in reverse, so that most recent event comes first
-    .groupBy('user) { group =>
-      group.sortBy('epoch).reverse
-        .scanLeft(('epoch, 'temp) -> ('originalEpoch, 'duration))((0L, 0L)) {
-          (firstLine: (Long, Long), secondLine: (Long, Long)) =>
-            var delta = firstLine._1 - secondLine._1
-            // scanLeft is initialised with (0L,0L) so first subtraction 
-            // will result into a negative number! 
-            if (delta < 0L) delta = -delta
-            (secondLine._1, delta)
-        }
-    }
-    // Remove lines introduced by scanLeft and discard helping symbols
-    .filter('epoch) { x: Any => x != null }
-    .discard('temp, 'originalEpoch)
-    // Order in ascending time
-    .groupBy('user) { group =>
-      group.sortBy('epoch)
-    }
-    // You can now remove most recent events where we are uncertain of time spent
-    .filter('duration) { x: Long => x < 10000L }
-    .debug
-    .write(Tsv("result2"))
 
 }
 
