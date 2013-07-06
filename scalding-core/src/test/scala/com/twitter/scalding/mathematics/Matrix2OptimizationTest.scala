d1656628b6ae3571e2b02bdbd39894d7caa2cec7
Tomas Tauber <s0943263@sms.ed.ac.uk>
2013-07-06 12:06:04 +0200

    Fixed the occasional ScalaCheck StackOverflow (very annoying in Scalding) by limiting the depth

--- scalding-core/src/test/scala/com/twitter/scalding/mathematics/Matrix2OptimizationTest.scala
+++ scalding-core/src/test/scala/com/twitter/scalding/mathematics/Matrix2OptimizationTest.scala
@@ -135,16 +135,16 @@ object Matrix2Props extends Properties("Matrix2") {
     }
   }
 
-  val genNode = for {
+  def genNode(depth: Int) = for {
     v <- arbitrary[Int]
     p <- Gen.choose(1, 10)
-    left <- genFormula
-    right <- genFormula
-  } yield if (v > 0) randomProduct(p) else Sum(left, right)
+    left <- genFormula(depth + 1)
+    right <- genFormula(depth + 1)
+  } yield if (depth > 5) randomProduct(p) else (if (v > 0) randomProduct(p) else Sum(left, right))
 
-  def genFormula: Gen[Matrix2] = oneOf(genNode, genLeaf((0, 0))._1)
+  def genFormula(depth: Int): Gen[Matrix2] = if (depth > 5) genLeaf((0, 0))._1 else (oneOf(genNode(depth + 1), genLeaf((0, 0))._1))
 
-  implicit def arbT: Arbitrary[Matrix2] = Arbitrary(genFormula)
+  implicit def arbT: Arbitrary[Matrix2] = Arbitrary(genFormula(0))
 
   val genProdSeq = for {
     v <- Gen.choose(1, 10)
