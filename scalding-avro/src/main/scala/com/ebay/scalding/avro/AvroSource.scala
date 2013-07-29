6fc372f6c6aa0e9e0691db7f3c5d342d411cf981
Chris Severs <csevers@ebay.com>
2013-07-29 13:14:18 -0700

    Fixed helper functions, added license info and README.

--- scalding-avro/src/main/scala/com/ebay/scalding/avro/AvroSource.scala
+++ scalding-avro/src/main/scala/com/ebay/scalding/avro/AvroSource.scala
@@ -1,4 +1,4 @@
-/*  Copyright 2012 eBay, inc.
+/*  Copyright 2013 eBay, inc.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
