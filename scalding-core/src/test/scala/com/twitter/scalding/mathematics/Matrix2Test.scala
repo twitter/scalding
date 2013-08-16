791cfb6cd79d67d466810f7ad114c4ea1a5937ea
Tomas Tauber <s0943263@sms.ed.ac.uk>
2013-08-16 14:14:26 +0200

    Polished the Matrix2Test (TypedTsv writing, no Pipe conversions)

--- scalding-core/src/test/scala/com/twitter/scalding/mathematics/Matrix2Test.scala
+++ scalding-core/src/test/scala/com/twitter/scalding/mathematics/Matrix2Test.scala
@@ -21,7 +21,7 @@ class Matrix2Sum(args: Args) extends Job(args) {
   val mat2 = MatrixLiteral(tp2, NoClue)
   
   val sum = mat1 + mat2
-  sum.toTypedPipe.toPipe(('x1, 'y1, 'v1)).write(Tsv("sum"))
+  sum.toTypedPipe.write(TypedTsv[(Int,Int,Double)]("sum"))
 }
 
 class Matrix2Sum3(args: Args) extends Job(args) {
@@ -36,7 +36,7 @@ class Matrix2Sum3(args: Args) extends Job(args) {
   val mat1 = MatrixLiteral(tp1, NoClue)
 
   val sum = mat1 + mat1
-  sum.toTypedPipe.toPipe(('x1, 'y1, 'v1)).write(Tsv("sum"))
+  sum.toTypedPipe.write(TypedTsv[(Int,Int,(Double, Double, Double))]("sum"))
 }
 
 class Matrix2SumChain(args: Args) extends Job(args) {
@@ -59,7 +59,7 @@ class Matrix2SumChain(args: Args) extends Job(args) {
   val mat3 = MatrixLiteral(tp3, NoClue)  
   
   val sum = mat1 + mat2 + mat3
-  sum.toTypedPipe.toPipe(('x1, 'y1, 'v1)).write(Tsv("sum"))
+  sum.toTypedPipe.write(TypedTsv[(Int,Int,Double)]("sum"))
 }
 
 class Matrix2Prod(args: Args) extends Job(args) {
@@ -74,7 +74,7 @@ class Matrix2Prod(args: Args) extends Job(args) {
   val mat1 = MatrixLiteral(tp1, NoClue)
 
   val gram = mat1 * mat1.transpose
-  gram.toTypedPipe.toPipe(('x1, 'y1, 'v1)).write(Tsv("product"))
+  gram.toTypedPipe.write(TypedTsv[(Int,Int,Double)]("product"))
 }
 
 class Matrix2ProdSum(args: Args) extends Job(args) {
@@ -93,7 +93,7 @@ class Matrix2ProdSum(args: Args) extends Job(args) {
   val mat2 = MatrixLiteral(tp2, NoClue)
 
   val gram = (mat1 * mat1.transpose) + mat2
-  gram.toTypedPipe.toPipe(('x1, 'y1, 'v1)).write(Tsv("product-sum"))
+  gram.toTypedPipe.write(TypedTsv[(Int,Int,Double)]("product-sum"))
 }
 
 class Matrix2PropJob(args: Args) extends Job(args) {
@@ -110,7 +110,7 @@ class Matrix2PropJob(args: Args) extends Job(args) {
   val tsv2 = TypedTsv[(Int,Double)]("col")
   val col = MatrixLiteral(TypedPipe.from(tsv2).map { case (idx, v) => (idx, (), v) }, NoClue)
   
-  mat.binarizeAs[Boolean].propagate(col).toTypedPipe.map { case (idx, x, v) => (idx, v) }.toPipe(('x2, 'v2)).write(Tsv("prop-col"))
+  mat.binarizeAs[Boolean].propagate(col).toTypedPipe.map { case (idx, x, v) => (idx, v) }.write(TypedTsv[(Int,Double)]("prop-col"))
   
 }
 
@@ -127,7 +127,7 @@ class Matrix2Cosine(args : Args) extends Job(args) {
 
   val matL2Norm = mat1.rowL2Normalize
   val cosine = matL2Norm * matL2Norm.transpose
-  cosine.toTypedPipe.toPipe(('x1, 'y1, 'v1)).write(Tsv("cosine"))
+  cosine.toTypedPipe.write(TypedTsv[(Int,Int,Double)]("cosine"))
 }
 
 class Matrix2Test extends Specification {
@@ -143,10 +143,10 @@ class Matrix2Test extends Specification {
 
   "A MatrixSum job" should {
     TUtil.printStack {
-      JobTest("com.twitter.scalding.mathematics.MatrixSum")
+      JobTest("com.twitter.scalding.mathematics.Matrix2Sum")
         .source(Tsv("mat1", ('x1, 'y1, 'v1)), List((1, 1, 1.0), (2, 2, 3.0), (1, 2, 4.0)))
         .source(Tsv("mat2", ('x2, 'y2, 'v2)), List((1, 3, 3.0), (2, 1, 8.0), (1, 2, 4.0)))
-        .sink[(Int, Int, Double)](Tsv("sum")) { ob =>
+        .sink[(Int, Int, Double)](TypedTsv[(Int,Int,Double)]("sum")) { ob =>
           "correctly compute sums" in {
             val pMap = toSparseMat(ob)
             pMap must be_==(Map((1, 1) -> 1.0, (1, 2) -> 8.0, (1, 3) -> 3.0, (2, 1) -> 8.0, (2, 2) -> 3.0))
@@ -161,7 +161,7 @@ class Matrix2Test extends Specification {
     TUtil.printStack {
       JobTest("com.twitter.scalding.mathematics.Matrix2Sum3")
         .source(Tsv("mat1", ('x1, 'y1, 'v1)), List((1,1,(1.0, 3.0, 5.0)),(2,2,(3.0, 2.0, 1.0)),(1,2,(4.0, 5.0, 2.0))))
-        .sink[(Int, Int, (Double, Double, Double))](Tsv("sum")) { ob =>
+        .sink[(Int, Int, (Double, Double, Double))](TypedTsv[(Int,Int,(Double, Double, Double))]("sum")) { ob =>
           "correctly compute sums" in {
             val pMap = toSparseMat(ob)
             pMap must be_==(Map((1,1)->(2.0, 6.0, 10.0), (2,2)->(6.0, 4.0, 2.0), (1,2)->(8.0, 10.0, 4.0)))
@@ -178,7 +178,7 @@ class Matrix2Test extends Specification {
         .source(Tsv("mat1", ('x1, 'y1, 'v1)), List((1, 1, 1.0), (2, 2, 3.0), (1, 2, 4.0)))
         .source(Tsv("mat2", ('x2, 'y2, 'v2)), List((1, 3, 3.0), (2, 1, 8.0), (1, 2, 4.0)))
         .source(Tsv("mat3", ('x3, 'y3, 'v3)), List((1, 3, 4.0), (2, 1, 1.0), (1, 2, 4.0)))
-        .sink[(Int, Int, Double)](Tsv("sum")) { ob =>
+        .sink[(Int, Int, Double)](TypedTsv[(Int,Int,Double)]("sum")) { ob =>
           "correctly compute sums" in {
             val pMap = toSparseMat(ob)
             pMap must be_==(Map((1, 1) -> 1.0, (1, 2) -> 12.0, (1, 3) -> 7.0, (2, 1) -> 9.0, (2, 2) -> 3.0))
@@ -193,7 +193,7 @@ class Matrix2Test extends Specification {
     TUtil.printStack {
       JobTest("com.twitter.scalding.mathematics.Matrix2Prod")
         .source(Tsv("mat1", ('x1, 'y1, 'v1)), List((1, 1, 1.0), (2, 2, 3.0), (1, 2, 4.0)))
-        .sink[(Int, Int, Double)](Tsv("product")) { ob =>
+        .sink[(Int, Int, Double)](TypedTsv[(Int,Int,Double)]("product")) { ob =>
           "correctly compute products" in {
             val pMap = toSparseMat(ob)
             pMap must be_==(Map((1, 1) -> 17.0, (1, 2) -> 12.0, (2, 1) -> 12.0, (2, 2) -> 9.0))
@@ -209,7 +209,7 @@ class Matrix2Test extends Specification {
       JobTest("com.twitter.scalding.mathematics.Matrix2ProdSum")
         .source(Tsv("mat1", ('x1, 'y1, 'v1)), List((1, 1, 1.0), (2, 2, 3.0), (1, 2, 4.0)))
         .source(Tsv("mat2", ('x2, 'y2, 'v2)), List((1, 1, 1.0), (1, 2, 1.0), (2, 1, 1.0), (2, 2, 1.0)))
-        .sink[(Int, Int, Double)](Tsv("product-sum")) { ob =>
+        .sink[(Int, Int, Double)](TypedTsv[(Int,Int,Double)]("product-sum")) { ob =>
           "correctly compute products" in {
             val pMap = toSparseMat(ob)
             pMap must be_==(Map((1, 1) -> 18.0, (1, 2) -> 13.0, (2, 1) -> 13.0, (2, 2) -> 10.0))
@@ -233,7 +233,7 @@ class Matrix2Test extends Specification {
         */
       .source(TypedTsv[(Int,Int,Int)]("graph"), List((0,1,1), (0,2,1), (1,2,1), (2,0,1)))
       .source(TypedTsv[(Int,Double)]("col"), List((0,1.0), (1,2.0), (2,4.0)))
-      .sink[(Int, Double)](Tsv("prop-col")) { ob =>
+      .sink[(Int, Double)](TypedTsv[(Int,Double)]("prop-col")) { ob =>
         "correctly propagate columns" in {
           ob.toMap must be_==(Map(0 -> 6.0, 1 -> 4.0, 2 -> 1.0))
         }
@@ -247,7 +247,7 @@ class Matrix2Test extends Specification {
     TUtil.printStack {
     JobTest("com.twitter.scalding.mathematics.Matrix2Cosine")
       .source(Tsv("mat1",('x1,'y1,'v1)), List((1,1,1.0),(2,2,3.0),(1,2,4.0)))
-      .sink[(Int,Int,Double)](Tsv("cosine")) { ob =>
+      .sink[(Int, Int, Double)](TypedTsv[(Int,Int,Double)]("cosine")) { ob =>
         "correctly compute cosine similarity" in {
           val pMap = toSparseMat(ob)
           pMap must be_==( Map((1,1)->1.0, (1,2)->0.9701425001453319, (2,1)->0.9701425001453319, (2,2)->1.0 ))
