d05d9ecb96dd06eb87f9a5b78d34358787a82085
P. Oscar Boykin <oscar@twitter.com>
2013-09-12 16:03:26 -0700

    Final? improvements to TypedPipe

--- scalding-core/src/main/scala/com/twitter/scalding/typed/ValuePipe.scala
+++ scalding-core/src/main/scala/com/twitter/scalding/typed/ValuePipe.scala
@@ -23,12 +23,13 @@ import cascading.flow.FlowDef
 object ValuePipe {
   implicit def toPipe[V](v: ValuePipe[V]): TypedPipe[V] = v.toPipe
 
-  def fold[T, U, V](l: ValuePipe[T], r: ValuePipe[U])(f: (T, U) => V) =
+  def fold[T, U, V](l: ValuePipe[T], r: ValuePipe[U])(f: (T, U) => V): ValuePipe[V] =
     new ComputedValue(l.toPipe.cross(r.toPipe).map{ case (lv, rv) => f(lv, rv) })
 
-  implicit def semigroup[T](implicit sg: Semigroup[T]) = new Semigroup[ValuePipe[T]] {
-    def plus(l: ValuePipe[T], r: ValuePipe[T]) = fold(l, r)(sg.plus)
-  }
+  implicit def semigroup[T](implicit sg: Semigroup[T]): Semigroup[ValuePipe[T]] =
+    new Semigroup[ValuePipe[T]] {
+      def plus(l: ValuePipe[T], r: ValuePipe[T]) = fold(l, r)(sg.plus)
+    }
 }
 
 /** ValuePipe is special case of a TypedPipe of just a single element.
@@ -40,7 +41,7 @@ sealed trait ValuePipe[+T] {
 }
 case class LiteralValue[T](value: T)(implicit val flowDef: FlowDef, mode: Mode) extends ValuePipe[T] {
   def map[U](fn: T => U) = LiteralValue(fn(value))
-  lazy val toPipe = TypedPipe.from(IterableSource(Iterable(value)))
+  lazy val toPipe = TypedPipe.from(Iterable(value))
 }
 case class ComputedValue[T](override val toPipe: TypedPipe[T]) extends ValuePipe[T] {
   def map[U](fn: T => U) = ComputedValue(toPipe.map(fn))
