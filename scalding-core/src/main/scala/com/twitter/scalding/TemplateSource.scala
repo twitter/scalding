476885eceafc227964c0d447e757f1146b775bb0
Stephen Lewchuk <lewchuk@inkling.com>
2013-12-17 17:50:34 -0800

    Add a TemplateSource class.
    This provides access to the cascading TemplateTaps through a scalding Source object. Follows the pattern of FileSource.

---
+++ scalding-core/src/main/scala/com/twitter/scalding/TemplateSource.scala
@@ -0,0 +1,130 @@
+/*
+Copyright 2013 Inkling, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package com.twitter.scalding
+
+import org.apache.hadoop.mapred.JobConf
+import org.apache.hadoop.mapred.RecordReader
+import org.apache.hadoop.mapred.OutputCollector
+
+import cascading.scheme.Scheme
+import cascading.scheme.hadoop.{ TextDelimited => CHTextDelimited }
+import cascading.scheme.hadoop.TextLine.Compress
+import cascading.tap.SinkMode
+import cascading.tap.Tap
+import cascading.tap.hadoop.Hfs
+import cascading.tap.hadoop.{ TemplateTap => HTemplateTap }
+import cascading.tap.local.FileTap
+import cascading.tap.local.{ TemplateTap => LTemplateTap }
+import cascading.tuple.Fields
+
+/**
+* This is a base class for template based output sources
+*/
+abstract class TemplateSource extends SchemedSource {
+
+  // The root of the templated output.
+  def basePath: String
+  // The template as a Formatter string.
+  def template: String
+  // The fields to apply to the template.
+  def pathFields: Fields = Fields.ALL
+  // Mode to handle conflicts with existing output.
+  val sinkMode: SinkMode = SinkMode.REPLACE
+
+  /**
+   * Creates the template tap.
+   *
+   * @param readOrWrite An AccessMode object indicating if this is a source or sink.
+   * @param mode A Mode object representing the mode the job is running in.
+   *
+   * @returns A cascading TemplateTap.
+   */
+  override def createTap(readOrWrite: AccessMode)(implicit mode: Mode): Tap[_, _, _] = {
+    readOrWrite match {
+      case Read => throw new InvalidSourceException("Cannot use TemplateSource for input")
+      case Write => {
+        System.err.println(readOrWrite)
+        System.err.println(mode)
+        mode match {
+          case Local(_) => {
+            val localTap = new FileTap(localScheme, basePath, sinkMode)
+            new LTemplateTap(localTap, template, pathFields)
+          }
+          case hdfsMode @ Hdfs(_, _) => {
+            val hfsTap = new Hfs(hdfsScheme, basePath, sinkMode)
+            new HTemplateTap(hfsTap, template, pathFields)
+          }
+          case hdfsTest @ HadoopTest(_, _) => {
+            val hfsTap = new Hfs(hdfsScheme, hdfsTest.getWritePathFor(this), sinkMode)
+            new HTemplateTap(hfsTap, template, pathFields)
+          }
+          case _ => TestTapFactory(this, hdfsScheme).createTap(readOrWrite)
+        }
+      }
+    }
+  }
+
+  /**
+   * Validates the taps, makes sure there are no nulls as the path or template.
+   *
+   * @param mode A Mode object representing the mode the job is running in.
+   */
+  override def validateTaps(mode: Mode): Unit = {
+    if (basePath == null) {
+      throw new InvalidSourceException("basePath cannot be null for TemplateTap")
+    } else if (template == null) {
+      throw new InvalidSourceException("template cannot be null for TemplateTap")
+    }
+  }
+}
+
+/**
+ * An implementation of TSV output, split over a template tap.
+ *
+ * @param basePath The root of the templated output.
+ * @param template The template to use.
+ * @param pathFields The set of fields to apply to the path.
+ * @param writeHeader Flag to indicate that the header should be written to the file.
+ * @param sinkMode The SinkMode this tap should use.
+ */
+case class TemplatedTsv(
+  override val basePath: String,
+  override val template: String,
+  override val pathFields: Fields = Fields.ALL,
+  override val writeHeader: Boolean = false,
+  override val sinkMode: SinkMode = SinkMode.REPLACE)
+    extends TemplateSource with DelimitedScheme
+
+/**
+ * An implementation of SequenceFile output, split over a template tap.
+ *
+ * @param basePath The root of the templated output.
+ * @param template The template to use.
+ * @param sequenceFields The set of fields to use for the sequence file.
+ * @param pathFields The set of fields to apply to the path.
+ * @param sinkMode The SinkMode this tap should use.
+ */
+case class TemplatedSequenceFile(
+  override val basePath: String,
+  override val template: String,
+  val sequenceFields: Fields = Fields.ALL,
+  override val pathFields: Fields = Fields.ALL,
+  override val sinkMode: SinkMode = SinkMode.REPLACE)
+    extends TemplateSource with SequenceFileScheme {
+
+  override val fields = sequenceFields
+}
+
