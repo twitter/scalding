56671659e6817e485a21ab5d6e3a3eda27394233
Avi Bryant <avi@stripe.com>
2014-01-27 11:05:17 -0800

    add some comments about maxReplicas math

--- scalding-core/src/main/scala/com/twitter/scalding/typed/Sketched.scala
+++ scalding-core/src/main/scala/com/twitter/scalding/typed/Sketched.scala
@@ -60,6 +60,9 @@ case class SketchJoined[K:Ordering,V,V2,R]
       sketchOpt.toList.flatMap{cms =>
         val maxPerReducer = (cms.totalCount / numReducers) * maxReducerFraction + 1
         val maxReplicas = (cms.frequency(left.hash(v._1)).estimate.toDouble / maxPerReducer)
+
+        //if the frequency is 0, maxReplicas.ceil will be 0 so we will filter out this key entirely
+        //if it's < maxPerReducer, the ceil will round maxReplicas up to 1 to ensure we still see it
         val replicas = fn(maxReplicas.ceil.toInt.min(numReducers))
         replicas.toList.map{i => (i,v._1) -> v._2}
       }
