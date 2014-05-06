72e5f4a67ed2722ded8db142cd2baeaac2bdd8b8
Jonathan Coveney <jcoveney@twitter.com>
2014-05-06 09:40:26 -0700

    Revert test_tutorials.sh

--- scripts/test_tutorials.sh
+++ scripts/test_tutorials.sh
@@ -2,8 +2,6 @@ set -e # first error should stop execution of this script
 
 SCALD="scripts/scald.rb --local"
 
-scripts/scald.rb --clean
-
 # Note: it might be preferable to have .travis.yml pass this as an argument
 if [ $TRAVIS_SCALA_VERSION ]; then
   echo "using TRAVIS_SCALA_VERSION ${TRAVIS_SCALA_VERSION}"
@@ -16,36 +14,36 @@ $SCALD tutorial/Tutorial2.scala
 
 $SCALD tutorial/Tutorial3.scala \
   --input tutorial/data/hello.txt
-
+ 
 $SCALD tutorial/Tutorial4.scala \
   --input tutorial/data/hello.txt \
   --output tutorial/data/output4.txt
-
+ 
 $SCALD tutorial/Tutorial5.scala \
   --input tutorial/data/hello.txt \
   --output tutorial/data/output5.txt \
   --words tutorial/data/words.txt
-
+ 
 $SCALD tutorial/MatrixTutorial0.scala \
   --input tutorial/data/graph.tsv \
   --output tutorial/data/outdegree.tsv
-
+ 
 $SCALD tutorial/MatrixTutorial1.scala \
   --input tutorial/data/graph.tsv \
   --output tutorial/data/cofollows.tsv
-
+ 
 $SCALD tutorial/MatrixTutorial2.scala \
   --input tutorial/data/graph.tsv \
   --maxOutdegree 1000 \
   --output tutorial/data/graphFiltered.tsv
-
+ 
 $SCALD tutorial/MatrixTutorial3.scala \
   --input1 tutorial/data/graph.tsv \
   --input2 tutorial/data/graph2.tsv \
   --intersection tutorial/data/intersection.tsv \
   --leftDiff tutorial/data/leftDiff.tsv \
   --rightDiff tutorial/data/rightDiff.tsv
-
+ 
 $SCALD tutorial/MatrixTutorial5.scala \
   --input tutorial/data/graph.tsv \
   --output tutorial/data/cosineSim.tsv
