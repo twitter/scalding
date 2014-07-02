5cb3605b8dac21cc5f7c7a9499fd2e3b8b6f49e1
Brandon Holt <bholt@twitter.com>
2014-07-02 14:32:27 -0700

    get rid of unnecessary implicit Manifest, TupleConverter, TupleSetters

--- scalding-repl/src/main/scala/com/twitter/scalding/ShellPipe.scala
+++ scalding-repl/src/main/scala/com/twitter/scalding/ShellPipe.scala
@@ -23,17 +23,15 @@ import collection.JavaConverters._
 import cascading.tuple.{ TupleEntry, Fields }
 import cascading.pipe.Each
 
-class TypedSequenceFile[T](path: String)(
-  implicit val mf: Manifest[T], tget: TupleGetter[T], tset: TupleSetter[T]) extends SequenceFile(path, 0) with Mappable[T] with TypedSink[T] {
-
+class TypedSequenceFile[T](path: String) extends SequenceFile(path, 0) with Mappable[T] with TypedSink[T] {
   override def converter[U >: T] =
-    TupleConverter.asSuperConverter[T, U](TupleConverter.singleConverter[T](tget))
-  override def setter[U <: T] = TupleSetter.asSubSetter[T, U](TupleSetter.singleSetter[T])
-
+    TupleConverter.asSuperConverter[T, U](TupleConverter.singleConverter[T])
+  override def setter[U <: T] =
+    TupleSetter.asSubSetter[T, U](TupleSetter.singleSetter[T])
 }
 
 object TypedSequenceFile {
-  def apply[T](path: String)(implicit mf: Manifest[T]): TypedSequenceFile[T] =
+  def apply[T](path: String): TypedSequenceFile[T] =
     new TypedSequenceFile[T](path)
 }
 
@@ -62,7 +60,7 @@ class ShellTypedPipe[T](pipe: TypedPipe[T]) {
    * Save snapshot of a typed pipe to a temporary sequence file.
    * @return A TypedPipe to a new Source, reading from the sequence file.
    */
-  def snapshot(implicit mf: Manifest[T]): TypedPipe[T] = {
+  def snapshot: TypedPipe[T] = {
 
     // come up with unique temporary filename
     // TODO: refactor into TemporarySequenceFile class
@@ -80,10 +78,10 @@ class ShellTypedPipe[T](pipe: TypedPipe[T]) {
   // TODO: add back `toList` based on `snapshot` this time
 
   // TODO: add `dump` to view contents without reading into memory
-  def toIterator(implicit mf: Manifest[T]): Iterator[T] = pipe match {
+  def toIterator: Iterator[T] = pipe match {
     // if this is just a Converter on a head pipe
     // (true for the first pipe on a source, e.g. a snapshot pipe)
-    case TypedPipeInst(p, fields, conv: Converter[T]) if p.getPrevious.isEmpty =>
+    case TypedPipeInst(p, fields, conv: Converter[_]) if p.getPrevious.isEmpty =>
       val srcs = flowDef.getSources
       if (srcs.containsKey(p.getName)) {
         val tap = srcs.get(p.getName)
@@ -96,8 +94,8 @@ class ShellTypedPipe[T](pipe: TypedPipe[T]) {
       pipe.snapshot.toIterator
   }
 
-  def toList(implicit mf: Manifest[T]): List[T] = toIterator.toList
+  def toList: List[T] = toIterator.toList
 
-  def dump(implicit mf: Manifest[T]): Unit = toIterator.foreach(println(_))
+  def dump: Unit = toIterator.foreach(println(_))
 
 }
