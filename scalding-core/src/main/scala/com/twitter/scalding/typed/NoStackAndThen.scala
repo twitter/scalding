4298469f0cef7dbdc96a942a0613fd58c0f5fb07
P. Oscar Boykin <oscar@twitter.com>
2014-07-23 12:24:38 -1000

    Fix 2.10 compile failure

--- scalding-core/src/main/scala/com/twitter/scalding/typed/NoStackAndThen.scala
+++ scalding-core/src/main/scala/com/twitter/scalding/typed/NoStackAndThen.scala
@@ -29,9 +29,9 @@ sealed trait NoStackAndThen[-A, +B] extends java.io.Serializable {
 
 object NoStackAndThen {
   def apply[A, B](fn: A => B): NoStackAndThen[A, B] = NoStackWrap(fn)
-  private sealed trait ReversedStack[A, B]
-  private case class EmptyStack[A, B](fn: A => B) extends ReversedStack[A, B]
-  private case class NonEmpty[A, B, C](head: A => B, rest: ReversedStack[B, C]) extends ReversedStack[A, C]
+  private sealed trait ReversedStack[-A, +B]
+  private case class EmptyStack[-A, +B](fn: A => B) extends ReversedStack[A, B]
+  private case class NonEmpty[-A, B, +C](head: A => B, rest: ReversedStack[B, C]) extends ReversedStack[A, C]
 
   // Just wraps a function
   private case class NoStackWrap[A, B](fn: A => B) extends NoStackAndThen[A, B] {
@@ -44,20 +44,24 @@ object NoStackAndThen {
      * Any call that changes types, we replace that type with Any. These casts
      * can never fail, due to the structure above.
      */
+    //private def reversed[T](toPush: NoStackAndThen[A, T], rest: ReversedStack[T, C]): ReversedStack[A, C] =
     @annotation.tailrec
-    private def reversed(toPush: NoStackAndThen[A, Any], rest: ReversedStack[Any, C]): ReversedStack[Any, C] =
+    private def reversed(toPush: NoStackAndThen[A, Any], rest: ReversedStack[Any, C]): ReversedStack[A, C] =
       toPush match {
         case NoStackWrap(fn) => NonEmpty(fn, rest)
-        case NoStackMore(more, fn) => reversed(more.asInstanceOf[NoStackAndThen[A, Any]], NonEmpty(fn, rest))
+        case NoStackMore(more, fn) => reversed(more, NonEmpty(fn, rest))
       }
+    //private def call[T](arg: T, revstack: ReversedStack[T, C]): C = revstack match {
     @annotation.tailrec
     private def call(arg: Any, revstack: ReversedStack[Any, C]): C = revstack match {
       case EmptyStack(last) => last(arg)
       case NonEmpty(head, rest) => call(head(arg), rest)
     }
     private lazy val revStack =
-      // casts needed for the tailrec
-      reversed(first.asInstanceOf[NoStackAndThen[A, Any]], EmptyStack(andThenFn.asInstanceOf[(Any) => (C)]))
+      // casts needed for the tailrec, they can't cause runtime errors
+      reversed(first, EmptyStack(andThenFn.asInstanceOf[(Any) => (C)]))
+        .asInstanceOf[ReversedStack[Any, C]]
+
     def apply(a: A): C = call(a, revStack)
   }
 }
