b45c6e0a591b5df7ef6160da591259d6b410813a
Ian O Connell <ioconnell@twitter.com>
2014-08-01 17:56:06 -0700

    Updates

--- scripts/build_assembly_no_test.sh
+++ scripts/build_assembly_no_test.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
 set -e # first error should stop execution of this script
 
 # Identify the bin dir in the distribution, and source the common include script
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
+TARGET=$1
 
 cd $BASE_DIR
 sed -i'' -e 's/\/\/ test in assembly/test in assembly/g' project/Build.scala
-./sbt ++$TRAVIS_SCALA_VERSION assembly &> /dev/null
+time ./sbt ++$TRAVIS_SCALA_VERSION $TARGET/assembly &> /dev/null
