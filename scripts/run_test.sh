c0bafcefecbfdafe33cc1078fbbb7d160a7d7ffe
Ian O Connell <ioconnell@twitter.com>
2014-08-01 22:03:29 -0700

    err

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,5 +1,3 @@
-set -e # first error should stop execution of this script
-
 # Identify the bin dir in the distribution, and source the common include script
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 cd $BASE_DIR
