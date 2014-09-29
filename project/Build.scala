c52c8da4c453e91d8d99854f995fe5e7e4353b57
Jonathan Coveney <jcoveney@twitter.com>
2014-09-29 18:17:01 -0400

    Remove unecessary option

--- project/Build.scala
+++ project/Build.scala
@@ -65,7 +65,7 @@ object ScaldingBuild extends Build {
 
     parallelExecution in Test := false,
 
-    scalacOptions ++= Seq("-unchecked", "-deprecation", "-language:implicitConversions", "-language:higherKinds", "-language:existentials", "-Xmax-classfile-name", "254"),
+    scalacOptions ++= Seq("-unchecked", "-deprecation", "-language:implicitConversions", "-language:higherKinds", "-language:existentials"),
 
     scalacOptions <++= (scalaVersion) map { sv =>
         if (sv startsWith "2.10")
