c82ab8cb09c804e5f0a85d69f862a689471be850
Ian O Connell <ioconnell@twitter.com>
2014-12-15 12:27:33 -0800

    Add a comment about restrictions on the type

--- scalding-macros/src/main/scala/com/twitter/scalding/macros/Macros.scala
+++ scalding-macros/src/main/scala/com/twitter/scalding/macros/Macros.scala
@@ -7,6 +7,8 @@ import com.twitter.scalding.macros.impl.MacroImpl
 import com.twitter.bijection.macros.IsCaseClass
 
 object Macros {
+  // These only work for simple types inside the case class
+  // Nested case classes are allowed, but only: Int, Boolean, String, Long, Short, Float, Double of other types are allowed
   def caseClassTupleSetter[T: IsCaseClass]: TupleSetter[T] = macro MacroImpl.caseClassTupleSetterImpl[T]
   def caseClassTupleConverter[T: IsCaseClass]: TupleConverter[T] = macro MacroImpl.caseClassTupleConverterImpl[T]
 }
