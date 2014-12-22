24ba0be18baa5cca273bb60523e41ce1d7d49853
Ian O Connell <ioconnell@twitter.com>
2014-12-22 17:13:29 -0500

    Add some failure case tests

--- scalding-jdbc-macros/src/test/scala/com/twitter/scalding/jdbc/macros/MacrosUnitTests.scala
+++ scalding-jdbc-macros/src/test/scala/com/twitter/scalding/jdbc/macros/MacrosUnitTests.scala
@@ -4,6 +4,7 @@ import org.scalatest.{ Matchers, WordSpec }
 
 import com.twitter.scalding._
 import com.twitter.scalding.jdbc.macros._
+import org.scalatest.exceptions.TestFailedException
 
 import com.twitter.bijection.macros.{ IsCaseClass, MacroGenerated }
 
@@ -17,7 +18,33 @@ case class User(
 
 class JdbcMacroUnitTests extends WordSpec with Matchers {
 
+  val dummy = new JDBCDescriptor[Nothing] {
+    override val columns = Nil
+  }
+
+  def isDescriptorAvailable[T](implicit proof: JDBCDescriptor[T] = dummy.asInstanceOf[JDBCDescriptor[T]]) {
+    proof shouldBe a[MacroGenerated]
+    proof.columns.isEmpty shouldBe false
+  }
+
+  "String field missing annotation" in {
+    case class BadUser(user_name: String, age: Int)
+    a[TestFailedException] should be thrownBy isDescriptorAvailable[BadUser]
+  }
+
+  "Option field with default" in {
+    case class BadUser(user_name: Option[String] = Some("bob"), age: Int)
+    a[TestFailedException] should be thrownBy isDescriptorAvailable[BadUser]
+  }
+
+  "Unknown field type" in {
+    case class BadUser(user_names: List[String])
+    a[TestFailedException] should be thrownBy isDescriptorAvailable[BadUser]
+  }
+
   "Produces the JdbcDescriptor" should {
+    isDescriptorAvailable[User]
+
     val expectedColumns = List(
       IntColumn("date_id", 32, None),
       StringColumn("user_name", 64, None),
