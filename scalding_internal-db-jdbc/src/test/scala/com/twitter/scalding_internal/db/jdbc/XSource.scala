5cbec3a231d044e68a61f929a4032acf262c833f
Ruban Monu <rmonu@twitter.com>
2015-02-02 10:34:11 -0800

    add missing test file

---
+++ scalding_internal-db-jdbc/src/test/scala/com/twitter/scalding_internal/db/jdbc/XSource.scala
@@ -0,0 +1,31 @@
+/*
+Copyright 2015 Twitter, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package com.twitter.scalding_internal.db.jdbc
+
+import com.twitter.scalding_internal.db._
+import com.twitter.scalding_internal.db.macros._
+
+// Macros don't work for certain case classes defined in a different file.
+// Possibly due to scala compilation order within packages.
+// Needs a fix.
+
+case class User(
+  date_id: Int,
+  @size(64) user_name: String,
+  age: Option[Int],
+  @size(22) gender: String = "male")
+
