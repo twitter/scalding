43266133b8de065c314cf42ed8210d4971ba809e
Ian O Connell <ioconnell@twitter.com>
2015-02-11 12:10:54 -0800

    Fixes test

--- scalding-core/src/main/scala/com/twitter/scalding/examples/WordCountJob.scala
+++ scalding-core/src/main/scala/com/twitter/scalding/examples/WordCountJob.scala
@@ -4,9 +4,9 @@ import com.twitter.scalding._
 
 class WordCountJob(args: Args) extends Job(args) {
   TypedPipe.from(TextLine(args("input")))
-    .flatMap { line  => line.split("\\s+") }
+    .flatMap { line => line.split("\\s+") }
     .map { word => (word, 1L) }
     .sumByKey
-     // The compiler will enforce the type coming out of the sumByKey is the same as the type we have for our sink
+    // The compiler will enforce the type coming out of the sumByKey is the same as the type we have for our sink
     .write(TypedTsv[(String, Long)](args("output")))
 }
