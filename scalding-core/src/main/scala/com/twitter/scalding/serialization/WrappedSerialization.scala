064c63373444b654da9acf22a8aa498806619617
P. Oscar Boykin <oscar@twitter.com>
2015-02-27 16:41:19 -1000

    use a Map rather than an Iterable for lookups

--- scalding-core/src/main/scala/com/twitter/scalding/serialization/WrappedSerialization.scala
+++ scalding-core/src/main/scala/com/twitter/scalding/serialization/WrappedSerialization.scala
@@ -32,7 +32,7 @@ class WrappedSerialization[T] extends HSerialization[T] with Configurable {
   import WrappedSerialization.ClassSerialization
 
   private var conf: Option[Configuration] = None
-  private var serializations: Iterable[ClassSerialization[_]] = Nil
+  private var serializations: Map[Class[_], Serialization[_]] = Map.empty
 
   override def getConf: Configuration = conf.get
   override def setConf(config: Configuration) {
@@ -40,11 +40,10 @@ class WrappedSerialization[T] extends HSerialization[T] with Configurable {
     serializations = WrappedSerialization.getBinary(config)
   }
 
-  def accept(c: Class[_]): Boolean =
-    serializations.exists { case (cls, _) => cls == c }
+  def accept(c: Class[_]): Boolean = serializations.contains(c)
 
   def getSerialization(c: Class[T]): Option[Serialization[T]] =
-    serializations.collectFirst { case (cls, b) if cls == c => b }
+    serializations.get(c)
       // This cast should never fail since we matched the class
       .asInstanceOf[Option[Serialization[T]]]
 
@@ -102,7 +101,7 @@ object WrappedSerialization {
   def setBinary(conf: Configuration, bufs: Iterable[ClassSerialization[_]]): Unit =
     rawSetBinary(bufs, { case (k, v) => conf.set(k, v) })
 
-  def getBinary(conf: Configuration): Iterable[ClassSerialization[_]] =
+  def getBinary(conf: Configuration): Map[Class[_], Serialization[_]] =
     conf
       .iterator
       .asScala
@@ -119,5 +118,5 @@ object WrappedSerialization {
               deser(conf.getClassByName(className))
             case _ => sys.error(s"ill formed bufferables: ${clsbuf}")
           }
-      }.toList
+      }.toMap
 }
