2e56b06169720f07e3926c3a8b152d65aa189f93
Eddie Xie <exie@twitter.com>
2015-04-11 17:33:29 -0700

    Make the splitter faster

--- scalding-core/src/main/scala/com/twitter/scalding/StringUtility.scala
+++ scalding-core/src/main/scala/com/twitter/scalding/StringUtility.scala
@@ -1,39 +1,25 @@
 package com.twitter.scalding
 
+import java.util
+
+import scala.collection.mutable.ArrayBuffer
+
 object StringUtility {
-  // Scala's split function uses regex which is too slow thus we create this faster function
-  private def isTrailingWith(text: String, separator: String) = {
-    val result = text.foldRight((List[Boolean](), true)) {
-      case (c, (tillNow, isTrailingBefore)) =>
-        val trailing = isTrailingBefore && (c.toString == separator)
-        (tillNow ++ List(trailing), trailing)
-    }
-    result._1.reverse
-  }
-
-  private def fastSplitHelper(text: String, key: String, isTrailingAt: List[Boolean]): Seq[String] = {
-    if (text.isEmpty) {
-      Seq()
-    } else {
-      val firstIndex = text.indexOf(key)
-      if (firstIndex == -1) {
-        Seq(text)
+  private def fastSplitHelper(text: String, key: String, from: Int, textLength: Int): List[String] = {
+    val firstIndex = text.indexOf(key, from)
+    if (firstIndex == -1) {
+      if (from < textLength) {
+        List(text.substring(from))
       } else {
-        // the text till the separator should be kept in any case
-        val currentChunk = text.substring(0, firstIndex)
-        // if it's trailing from this index, then ignore the rest of the string
-        if (isTrailingAt(firstIndex)) {
-          Seq(currentChunk)
-        } else {
-          // Otherwise, we want to see the rest of the string
-          val nextText = text.substring(firstIndex + 1)
-          Seq(currentChunk) ++ fastSplitHelper(nextText, key, isTrailingAt.slice(firstIndex + 1, text.length))
-        }
+        List()
       }
+    } else {
+      // the text till the separator should be kept in any case
+      text.substring(from, firstIndex) :: fastSplitHelper(text, key, firstIndex + 1, textLength)
     }
-
   }
+
   def fastSplit(text: String, key: String): Seq[String] = {
-    fastSplitHelper(text, key, isTrailingWith(text, key))
+    fastSplitHelper(text, key, 0, text.length)
   }
 }
