5f8f3f2874d762f5b2e5b53e0375067d2f8921dd
Eddie Xie <exie@twitter.com>
2015-04-14 18:08:45 -0700

    Deal with multiple characters separator

--- scalding-core/src/main/scala/com/twitter/scalding/StringUtility.scala
+++ scalding-core/src/main/scala/com/twitter/scalding/StringUtility.scala
@@ -1,25 +1,21 @@
 package com.twitter.scalding
 
-import java.util
-
-import scala.collection.mutable.ArrayBuffer
-
 object StringUtility {
-  private def fastSplitHelper(text: String, key: String, from: Int, textLength: Int): List[String] = {
+  private def fastSplitHelper(text: String, key: String, from: Int, textLength: Int, keyLength: Int): List[String] = {
     val firstIndex = text.indexOf(key, from)
     if (firstIndex == -1) {
       if (from < textLength) {
         List(text.substring(from))
       } else {
-        List()
+        List("")
       }
     } else {
       // the text till the separator should be kept in any case
-      text.substring(from, firstIndex) :: fastSplitHelper(text, key, firstIndex + 1, textLength)
+      text.substring(from, firstIndex) :: fastSplitHelper(text, key, firstIndex + keyLength, textLength, keyLength)
     }
   }
 
-  def fastSplit(text: String, key: String): Seq[String] = {
-    fastSplitHelper(text, key, 0, text.length)
+  def fastSplit(text: String, key: String): List[String] = {
+    fastSplitHelper(text, key, 0, text.length, key.length)
   }
 }
