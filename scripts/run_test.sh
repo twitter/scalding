893287f0eee26bf579ae5a35019d51387e1566e5
Ian O Connell <ioconnell@twitter.com>
2015-04-17 08:58:24 -0700

    Remove a plugin we don't need, adds to build time. Bump to newer SBT that has some resolution caching. Keep its cache dir and that of maven

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -2,19 +2,26 @@
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 cd $BASE_DIR
 
-export JVM_OPTS="-XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC -XX:ReservedCodeCacheSize=96m -XX:+TieredCompilation -XX:MaxPermSize=128m -Xms256m -Xmx512m -Xss2m"
+export JVM_OPTS="-XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC -XX:ReservedCodeCacheSize=96m -XX:+TieredCompilation -XX:MaxPermSize=256m -Xms256m -Xmx2048m -Xss2m"
 
 INNER_JAVA_OPTS="set javaOptions += \"-Dlog4j.configuration=file://$TRAVIS_BUILD_DIR/project/travis-log4j.properties\""
 
 withCmd() {
   CMD=$1
-  for t in $TEST_TARGET; do echo "; project $t; set logLevel := Level.Warn; $INNER_JAVA_OPTS; ++$TRAVIS_SCALA_VERSION; $CMD"; done
+  for t in $TEST_TARGET; do
+    echo "Running: ./sbt ; project $t; set logLevel := Level.Warn; $INNER_JAVA_OPTS; ++$TRAVIS_SCALA_VERSION; $CMD"
+    time ./sbt "; project $t; set logLevel := Level.Warn; $INNER_JAVA_OPTS; ++$TRAVIS_SCALA_VERSION; $CMD" &> /dev/null
+  done
 }
 
 bash -c "while true; do echo -n .; sleep 5; done" &
 
 PROGRESS_REPORTER_PID=$!
-time ./sbt "$(withCmd "compile; test:compile")" &> /dev/null
+time withCmd "compile; test:compile"
 kill -9 $PROGRESS_REPORTER_PID
 
-./sbt "$(withCmd test)"
+# Separate JVM's while slower, avoids GC issues in sbt itself
+for t in $TEST_TARGET; do
+  echo "Running: ./sbt ; project $t; set logLevel := Level.Warn; $INNER_JAVA_OPTS; ++$TRAVIS_SCALA_VERSION; test"
+  time ./sbt "; project $t; set logLevel := Level.Warn; $INNER_JAVA_OPTS; ++$TRAVIS_SCALA_VERSION; test" || exit 1
+done
