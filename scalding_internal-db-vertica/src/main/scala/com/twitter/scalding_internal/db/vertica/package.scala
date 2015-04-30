88e9260625f78992eb54fa6951923a9602babc95
Ruban Monu <rmonu@twitter.com>
2015-04-30 15:15:00 -0700

    remove jdbc and vertica subprojects for now. those will be merged with oss separately

--- scalding_internal-db-vertica/src/main/scala/com/twitter/scalding_internal/db/vertica/package.scala
+++
@@ -1,7 +0,0 @@
-package com.twitter.scalding_internal.db
-import scala.language.experimental.{ macros => smacros }
-import com.twitter.scalding_internal.db.vertica.VerticaRowSerializer
-
-package object vertica {
-  implicit def apply[T]: VerticaRowSerializer[T] = macro com.twitter.scalding_internal.db.vertica.macros.VerticaRowSerializerProviderImpl[T]
-}
