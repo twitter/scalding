e90edc18a1d25e9dffa94a1909cebeebd4058dd0
Joe Nievelt <jnievelt@twitter.com>
2015-05-14 13:18:29 -0700

    Prepare for release of 0.14.0

--- scalding-core/src/main/scala/com/twitter/package.scala
+++ scalding-core/src/main/scala/com/twitter/package.scala
@@ -34,7 +34,7 @@ package object scalding {
   /**
    * Make sure this is in sync with version.sbt
    */
-  val scaldingVersion: String = "0.13.1"
+  val scaldingVersion: String = "0.14.0"
 
   object RichPathFilter {
     implicit def toRichPathFilter(f: PathFilter) = new RichPathFilter(f)
