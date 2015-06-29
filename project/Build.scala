f283c25b134dd1d38f401e431c2df8089477d94e
Ian O Connell <ioconnell@twitter.com>
2015-06-29 10:53:26 -0700

    Re-order releases above snapshots, add maven central main repo

--- project/Build.scala
+++ project/Build.scala
@@ -68,8 +68,9 @@ object ScaldingBuild extends Build {
 
     resolvers ++= Seq(
       "Local Maven Repository" at "file://" + Path.userHome.absolutePath + "/.m2/repository",
-      "snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+      "maven central" at "https://repo.maven.apache.org/maven2",
       "releases" at "https://oss.sonatype.org/content/repositories/releases",
+      "snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
       "Concurrent Maven Repo" at "http://conjars.org/repo",
       "Clojars Repository" at "http://clojars.org/repo",
       "Twitter Maven" at "http://maven.twttr.com",
