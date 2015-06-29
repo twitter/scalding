5c14140a36e5468980001c76f6f5d1e865cb9cf8
Ian O Connell <ioconnell@twitter.com>
2015-06-29 13:15:49 -0700

    Scripts updates again

--- scripts/packDeps.sh
+++ scripts/packDeps.sh
@@ -6,7 +6,7 @@ ORIG_PATH=`pwd`
 PROGRESS_REPORTER_PID=$!
 DEP_CACHE_PATH=$HOME/.scalding.dep.cache
 mkdir -p $DEP_CACHE_PATH
-
+ID=$(echo $TEST_TARGET | md5deep)
 # cd $ORIG_PATH
 # tar zcf ~/.ivy2/cache/scalding-cache.tar.gz project/target
 
@@ -17,18 +17,18 @@ tar cf $DEP_CACHE_PATH/cache-new.tar cache
 
 cd $DEP_CACHE_PATH
 
-if [ -f cache.tar ]; then
-  OLD_MD5=`md5deep -q cache.tar`
+if [ -f cache-$ID.tar ]; then
+  OLD_MD5=`md5deep -q cache-$ID.tar`
   NEW_MD5=`md5deep -q cache-new.tar`
   if [ $OLD_MD5 == $NEW_MD5 ]; then
     echo "Same, not updating"
     rm cache-new.tar
   else
     echo "Changes detected... OLD: $OLD_MD5 , NEW: $NEW_MD5"
-    mv cache-new.tar cache.tar
+    mv cache-new.tar cache-$ID.tar
   fi
 else
-  mv cache-new.tar cache.tar
+  mv cache-new.tar cache-$ID.tar
 fi
 
 kill -9 $PROGRESS_REPORTER_PID
--- scripts/unpackDeps.sh
+++ scripts/unpackDeps.sh
@@ -4,21 +4,22 @@ PROGRESS_REPORTER_PID=$!
 
 DEP_CACHE_PATH=$HOME/.scalding.dep.cache
 ORIG_PATH=`pwd`
-if [ -f $DEP_CACHE_PATH/cache.tar ]; then
+ID=$(echo $TEST_TARGET | md5deep)
+if [ -f $DEP_CACHE_PATH/cache-$ID.tar ]; then
   echo "Found cache, unpacking it"
   mkdir -p ~/.ivy2
   cd ~/.ivy2
-  tar xf $DEP_CACHE_PATH/cache.tar
+  tar xf $DEP_CACHE_PATH/cache-$ID.tar
   echo "Unpacked deps"
 else
   echo "No deps found, trying a dropbox link of ianoc's"
   cd $DEP_CACHE_PATH
 
   URL=$(curl https://www.dropbox.com/s/ggmfx38vmbzz0q8/cache.tar?dl=0 2> /dev/null | grep 'https://dl.dropboxusercontent.com' | sed -e's/.*href="\([^"]*\)".*/\1/g')
-  time curl -o cache.tar $URL && \
+  time curl -o cache-$ID.tar $URL && \
   mkdir -p ~/.ivy2  && \
   cd ~/.ivy2 && \
-  tar xf $DEP_CACHE_PATH/cache.tar
+  tar xf $DEP_CACHE_PATH/cache-$ID.tar
 fi
 
 if [ -f ~/.ivy2/cache/scalding-cache.tar.gz ]; then
