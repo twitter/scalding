e93f3f26e4ee32e0470ba98c8965394bbebd90a3
Ian O Connell <ioconnell@twitter.com>
2015-06-29 15:03:22 -0700

    Back to old behavior for now

--- scripts/packDeps.sh
+++ scripts/packDeps.sh
@@ -6,30 +6,28 @@ ORIG_PATH=`pwd`
 PROGRESS_REPORTER_PID=$!
 DEP_CACHE_PATH=$HOME/.scalding.dep.cache
 mkdir -p $DEP_CACHE_PATH
-ID=$(echo $TEST_TARGET | md5deep)
-# cd $ORIG_PATH
-# tar zcf ~/.ivy2/cache/scalding-cache.tar.gz project/target
+
+cd $ORIG_PATH
+tar zcf ~/.ivy2/cache/scalding-cache.tar.gz project/target
 
 cd ~/.ivy2
 
-rm -f cache/*.properties cache/ivy-report*
-find cache -name '*.jar' -exec rm -f {} \;
 tar cf $DEP_CACHE_PATH/cache-new.tar cache
 
 cd $DEP_CACHE_PATH
 
-if [ -f cache-$ID.tar ]; then
-  OLD_MD5=`md5deep -q cache-$ID.tar`
+if [ -f cache.tar ]; then
+  OLD_MD5=`md5deep -q cache.tar`
   NEW_MD5=`md5deep -q cache-new.tar`
   if [ $OLD_MD5 == $NEW_MD5 ]; then
     echo "Same, not updating"
     rm cache-new.tar
   else
     echo "Changes detected... OLD: $OLD_MD5 , NEW: $NEW_MD5"
-    mv cache-new.tar cache-$ID.tar
+    mv cache-new.tar cache.tar
   fi
 else
-  mv cache-new.tar cache-$ID.tar
+  mv cache-new.tar cache.tar
 fi
 
 kill -9 $PROGRESS_REPORTER_PID
--- scripts/unpackDeps.sh
+++ scripts/unpackDeps.sh
@@ -4,27 +4,26 @@ PROGRESS_REPORTER_PID=$!
 
 DEP_CACHE_PATH=$HOME/.scalding.dep.cache
 ORIG_PATH=`pwd`
-ID=$(echo $TEST_TARGET | md5deep)
-if [ -f $DEP_CACHE_PATH/cache-$ID.tar ]; then
+if [ -f $DEP_CACHE_PATH/cache.tar ]; then
   echo "Found cache, unpacking it"
   mkdir -p ~/.ivy2
   cd ~/.ivy2
-  tar xf $DEP_CACHE_PATH/cache-$ID.tar
+  tar xf $DEP_CACHE_PATH/cache.tar
   echo "Unpacked deps"
 else
   echo "No deps found, trying a dropbox link of ianoc's"
   cd $DEP_CACHE_PATH
 
   URL=$(curl https://www.dropbox.com/s/ggmfx38vmbzz0q8/cache.tar?dl=0 2> /dev/null | grep 'https://dl.dropboxusercontent.com' | sed -e's/.*href="\([^"]*\)".*/\1/g')
-  time curl -o cache-$ID.tar $URL && \
+  time curl -o cache.tar $URL && \
   mkdir -p ~/.ivy2  && \
   cd ~/.ivy2 && \
-  tar xf $DEP_CACHE_PATH/cache-$ID.tar
+  tar xf $DEP_CACHE_PATH/cache.tar
 fi
 
 if [ -f ~/.ivy2/cache/scalding-cache.tar.gz ]; then
   cd $ORIG_PATH
-  # tar zxf ~/.ivy2/cache/scalding-cache.tar.gz
+  tar zxf ~/.ivy2/cache/scalding-cache.tar.gz
   rm ~/.ivy2/cache/scalding-cache.tar.gz
 fi
 
