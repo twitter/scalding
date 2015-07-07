2d3e23f2141f959e7d317a3b2b70d079ea08fed9
Sidharth Kapur <skapur@twitter.com>
2015-07-07 13:23:54 -0700

    Remove comment

--- scalding-core/src/test/scala/com/twitter/scalding/source/typedtext/TypedTextTest.scala
+++ scalding-core/src/test/scala/com/twitter/scalding/source/typedtext/TypedTextTest.scala
@@ -2,14 +2,12 @@ package com.twitter.scalding.source.typedtext
 
 import com.twitter.scalding._
 
-//import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
 
 case class Test1(a: Int, b: Long, c: Option[Double])
 case class Test2(one: Test1, d: String)
 
-//@RunWith(classOf[JUnitRunner])
 class TypedTextTest extends FunSuite {
   test("Test with a flat tuple") {
     val source = TypedText.tsv[Test1]("myPath")
