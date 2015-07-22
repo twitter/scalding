d941aee22613287e0483d6c83afd575408111f94
Mansur Ashraf <mansoor.ashraf@gmail.com>
2015-07-22 12:00:05 -0700

    added comments

--- scalding-thrift-macros/src/main/scala/com/twitter/scalding/thrift/macros/RequiredBinaryComparators.scala
+++ scalding-thrift-macros/src/main/scala/com/twitter/scalding/thrift/macros/RequiredBinaryComparators.scala
@@ -5,6 +5,8 @@ import com.twitter.scalding.thrift.macros.impl.ScroogeInternalOrderedSerializati
 import scala.language.experimental.{ macros => smacros }
 
 /**
+ * Provides support for Scrooge classes in addition to primitives, cases classes, tuples etc. Use this
+ * if you use Scrooge classes as `key` in your scalding job.
  * @author Mansur Ashraf.
  */
 trait RequiredBinaryComparators extends RequiredBinaryComparatorsConfig {
