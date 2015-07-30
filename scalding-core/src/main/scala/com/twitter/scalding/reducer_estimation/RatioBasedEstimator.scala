d7715cfb03d38e1bf2dbbfbb747e4a6e7d428322
Ruban Monu <rmonu@twitter.com>
2015-07-30 15:59:23 -0700

    Handle no history case in RatioBasedEstimator

--- scalding-core/src/main/scala/com/twitter/scalding/reducer_estimation/RatioBasedEstimator.scala
+++ scalding-core/src/main/scala/com/twitter/scalding/reducer_estimation/RatioBasedEstimator.scala
@@ -48,6 +48,9 @@ abstract class RatioBasedEstimator extends ReducerEstimator {
     val threshold = RatioBasedEstimator.getInputRatioThreshold(conf)
 
     historyService.fetchHistory(info, maxHistory) match {
+      case Success(Nil) =>
+        LOG.warn("No matching history found.")
+        None
       case Success(history) =>
         val inputBytes = Common.totalInputSize(info.step)
 
