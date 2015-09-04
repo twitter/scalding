ba9cbdacaf3cfee24d289f8fb30148e0bf1eeffd
Oleksii Iepishkin <epishkin@gmail.com>
2015-09-04 15:42:32 -0400

    make getBytesPerReducer support human readable values like 128m and 1g

--- scalding-core/src/main/scala/com/twitter/scalding/reducer_estimation/InputSizeReducerEstimator.scala
+++ scalding-core/src/main/scala/com/twitter/scalding/reducer_estimation/InputSizeReducerEstimator.scala
@@ -1,8 +1,5 @@
 package com.twitter.scalding.reducer_estimation
 
-import scala.collection.JavaConverters._
-import cascading.flow.FlowStep
-import cascading.tap.{ Tap, CompositeTap }
 import cascading.tap.hadoop.Hfs
 import org.apache.hadoop.mapred.JobConf
 import org.slf4j.LoggerFactory
@@ -11,14 +8,22 @@ object InputSizeReducerEstimator {
   val BytesPerReducer = "scalding.reducer.estimator.bytes.per.reducer"
   val defaultBytesPerReducer = 1L << 32 // 4 GB
 
-  /** Get the target bytes/reducer from the JobConf */
-  def getBytesPerReducer(conf: JobConf): Long = conf.getLong(BytesPerReducer, defaultBytesPerReducer)
+  /**
+   * Get the target bytes/reducer from the JobConf.
+   * Supported formats are <code>long</code> or human readable format.
+   * For human readable format you can use the following suffix (case insensitive):
+   * k(kilo), m(mega), g(giga), t(tera), p(peta), e(exa).
+   *
+   * Examples: 1024, 128m, 1g.
+   */
+  def getBytesPerReducer(conf: JobConf): Long =
+    conf.getLongBytes(BytesPerReducer, defaultBytesPerReducer)
 }
 
 /**
  * Estimator that uses the input size and a fixed "bytesPerReducer" target.
  *
- * Bytes per reducer can be configured with configuration parameter, defaults to 1 GB.
+ * Bytes per reducer can be configured with configuration parameter, defaults to 4 GB.
  */
 class InputSizeReducerEstimator extends ReducerEstimator {
 
