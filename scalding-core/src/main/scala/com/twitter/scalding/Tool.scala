88f91b8966df18424e52c60df4c6a56ae2182f00
Gabriel Gonzalez <ggonzalez@twitter.com>
2016-02-08 13:08:25 -0800

    Prefer pattern guard in `Tool.scala`

--- scalding-core/src/main/scala/com/twitter/scalding/Tool.scala
+++ scalding-core/src/main/scala/com/twitter/scalding/Tool.scala
@@ -40,15 +40,13 @@ class Tool extends Configured with HTool {
 
   protected def getJob(args: Args): Job = rootJob match {
     case Some(job) => job(args)
-    case None =>
-      if (args.positional.isEmpty) {
-        throw ArgsException("Usage: Tool <jobClass> --local|--hdfs [args...]")
-      } else {
-        val jobName = args.positional(0)
-        // Remove the job name from the positional arguments:
-        val nonJobNameArgs = args + ("" -> args.positional.tail)
-        Job(jobName, nonJobNameArgs)
-      }
+    case None if args.positional.isEmpty =>
+      throw ArgsException("Usage: Tool <jobClass> --local|--hdfs [args...]")
+    case None => // has at least one arg
+      val jobName = args.positional(0)
+      // Remove the job name from the positional arguments:
+      val nonJobNameArgs = args + ("" -> args.positional.tail)
+      Job(jobName, nonJobNameArgs)
   }
 
   // This both updates the jobConf with hadoop arguments
