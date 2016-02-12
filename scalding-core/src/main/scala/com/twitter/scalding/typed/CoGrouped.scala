46aacdb62713b46eb2f614248679a2b2877e2534
Gabriel Gonzalez <ggonzalez@twitter.com>
2016-02-12 09:51:32 -0800

    Correct one of the explanatory comments

--- scalding-core/src/main/scala/com/twitter/scalding/typed/CoGrouped.scala
+++ scalding-core/src/main/scala/com/twitter/scalding/typed/CoGrouped.scala
@@ -333,7 +333,7 @@ abstract class CoGroupedJoiner[K](inputSize: Int,
 
   override def getIterator(jc: JoinerClosure) = {
     val iters = (0 until distinctSize).map { jc.getIterator(_).asScala.buffered }
-    // Don't pattern match in performance-critical code
+    // This use of `_.get` is safe, but difficult to prove in the types.
     @SuppressWarnings(Array("org.brianmckenna.wartremover.warts.OptionPartial"))
     val keyTuple = iters
       .collectFirst { case iter if iter.nonEmpty => iter.head }
