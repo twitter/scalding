bce1bd8eb455049d814c3aade372f01f944e103d
Ian O Connell <ianoc@stripe.com>
2016-02-15 17:56:21 -0800

    Use new helpers for execution test

--- scalding-core/src/test/scala/com/twitter/scalding/ExecutionTest.scala
+++ scalding-core/src/test/scala/com/twitter/scalding/ExecutionTest.scala
@@ -396,7 +396,7 @@ class ExecutionTest extends WordSpec with Matchers {
 
       val result = Execution.withParallelism(executions, 3)
 
-      assert(result.waitFor(Config.default, Local(true)).get == 0.to(10).toSeq)
+      result.shouldSucceed() == 0.to(10).toSeq
     }
 
     "block correctly" in {
