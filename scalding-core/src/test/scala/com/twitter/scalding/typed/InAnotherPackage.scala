a76865d69948a2fef0953b8a061a95da9c070bf0
Oscar Boykin <oscar.boykin@gmail.com>
2017-04-17 14:54:29 -1000

    Add a hack to fix a failing test

--- scalding-core/src/test/scala/com/twitter/scalding/typed/InAnotherPackage.scala
+++ scalding-core/src/test/scala/com/twitter/scalding/typed/InAnotherPackage.scala
@@ -7,8 +7,16 @@ import SExecutionContext.Implicits.global
 object InAnotherPackage {
   def buildF: Future[TypedPipe[(Int, Int)]] = {
     Future {
-      TypedPipe.from(List(1, 2, 3, 4, 555, 3))
+      val p = TypedPipe.from(List(1, 2, 3, 4, 555, 3))
         .map { case x => (x, x) }
+
+      // TODO this shouldn't be needed. In a following
+      // PR, this should be removed if we merge this
+      // change
+      LineNumber.tryNonScaldingCaller match {
+        case None => p
+        case Some(c) => p.withDescription(c.toString)
+      }
     }
   }
 }
