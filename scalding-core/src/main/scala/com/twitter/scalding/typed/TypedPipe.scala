6bcb16949bf3ffb95b0023be514151956db31162
Timur <abishev.timur@gmail.com>
2017-05-22 14:10:48 -0700

    Cherry-pick "Fix stack overflow in typedPipeMonoid.zero" (#1688)
    * Fix stack overflow in typedPipeMonoid.zero
    
    * Timur's comments.

--- scalding-core/src/main/scala/com/twitter/scalding/typed/TypedPipe.scala
+++ scalding-core/src/main/scala/com/twitter/scalding/typed/TypedPipe.scala
@@ -102,7 +102,7 @@ object TypedPipe extends Serializable {
    * TypedPipe instances are monoids. They are isomorphic to multisets.
    */
   implicit def typedPipeMonoid[T]: Monoid[TypedPipe[T]] = new Monoid[TypedPipe[T]] {
-    def zero = empty
+    def zero = TypedPipe.empty
     def plus(left: TypedPipe[T], right: TypedPipe[T]): TypedPipe[T] =
       left ++ right
   }
