1994dfcfc41e348bd49334ef5c9d9cf0a23be921
Oscar Boykin <oscar.boykin@gmail.com>
2018-02-02 17:42:52 -1000

    Make scalding native MemoryBackend usable

---
+++ scalding-core/src/main/scala/com/twitter/scalding/typed/memory_backend/AtomicBox.scala
@@ -0,0 +1,30 @@
+package com.twitter.scalding.typed.memory_backend
+
+import java.util.concurrent.atomic.AtomicReference
+
+class AtomicBox[T <: AnyRef](init: T) {
+  private[this] val ref = new AtomicReference[T](init)
+
+  def lazySet(t: T): Unit =
+    ref.lazySet(t)
+
+  /**
+   * use a pure function to update the state.
+   * fn may be called more than once
+   */
+  def update[R](fn: T => (T, R)): R = {
+
+    @annotation.tailrec
+    def loop(): R = {
+      val init = ref.get
+      val (next, res) = fn(init)
+      if (ref.compareAndSet(init, next)) res
+      else loop()
+    }
+
+    loop()
+  }
+
+  def get(): T = ref.get
+}
+
