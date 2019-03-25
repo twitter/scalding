5037c8617315568af1037f971391631488c3c506
stephbian <stephbian@users.noreply.github.com>
2019-03-25 15:30:52 -0700

    Add configurable partition count for spark (#1903)
    * Add configurable partition count for spark
    
    * address comments
    
    * fix tests

--- scalding-spark/src/main/scala/com/twitter/scalding/spark_backend/SparkBackend.scala
+++ scalding-spark/src/main/scala/com/twitter/scalding/spark_backend/SparkBackend.scala
@@ -11,6 +11,52 @@ import scala.collection.mutable.{ Map => MMap, ArrayBuffer }
 
 object SparkPlanner {
   import SparkMode.SparkConfigMethods
+  sealed trait PartitionComputer {
+    def apply(currentNumPartitions: Int): Int
+  }
+
+  final case object IdentityPartitionComputer extends PartitionComputer {
+    def apply(currentNumPartitions: Int): Int = currentNumPartitions
+  }
+
+  /**
+   * A PartitionComputer which returns the desired number of partitions given the configured
+   * max partition count, reducer scaling factor, number of scalding reducers, and current number of partitions.
+   * We calculate the desired number of partitions in two stages:
+   * 1. If the number of scalding reducers is provided, we scale this number by the reducer scaling factor.
+   *    If it is <= 0 or missing, we use the current number of partitions.
+   * 2. If we have a configured a max number of partitions, we cap the result of 1 by this number. Otherwise,
+   *    just return the result of 1.
+   */
+  final case class ConfigPartitionComputer(config: Config, scaldingReducers: Option[Int]) extends PartitionComputer {
+    def apply(currentNumPartitions: Int): Int = {
+      val maxPartitions = config.getMaxPartitionCount
+      val getReducerScaling = config.getReducerScaling.getOrElse(1.0D)
+      val candidate = scaldingReducers match {
+        case None =>
+          currentNumPartitions
+        case Some(i) if i <= 0 =>
+          // scaldingReducers should be > 0, otherwise we default to the current number of partitions
+          currentNumPartitions
+        case Some(red) =>
+          (getReducerScaling * red).toInt
+      }
+      if (candidate > 0) {
+        maxPartitions match {
+          case Some(maxP) =>
+            Math.min(maxP, candidate)
+          case None =>
+            candidate
+        }
+      } else if (candidate == 0) {
+        // we probably always want at least 1 partition
+        1
+      } else {
+        throw new IllegalArgumentException("Got a negative partition count. Check configured maxPartitionCount or reducerScaling.")
+      }
+    }
+  }
+
   /**
    * Convert a TypedPipe to an RDD
    */
@@ -88,8 +134,10 @@ object SparkPlanner {
           val op = rec(input) // linter:disable:UndesirableTypeInference
           op.map(fn)
         case (m @ MergedTypedPipe(_, _), rec) =>
-          def go[A](m: MergedTypedPipe[A]): Op[A] =
-            rec(m.left) ++ rec(m.right)
+          def go[A](m: MergedTypedPipe[A]): Op[A] = {
+            val pc = ConfigPartitionComputer(config, None)
+            rec(m.left).merge(pc, rec(m.right))
+          }
           go(m)
         case (SourcePipe(src), _) =>
           Op.Source(config, src, srcs(src))
@@ -149,16 +197,19 @@ object SparkPlanner {
           def go[K, V1, V2](uir: IdentityValueSortedReduce[K, V1, V2]): Op[(K, V2)] = {
             type OpT[V] = Op[(K, V)]
             val op = rec(uir.mapped)
-            val sortedOp = op.sorted(uir.keyOrdering, uir.valueSort)
+            val pc = ConfigPartitionComputer(config, uir.reducers)
+            val sortedOp = op.sorted(pc)(uir.keyOrdering, uir.valueSort)
             uir.evidence.subst[OpT](sortedOp)
           }
           go(ivsr)
-        case (ReduceStepPipe(ValueSortedReduce(ordK, pipe, ordV, fn, _, _)), rec) =>
+        case (ReduceStepPipe(ValueSortedReduce(ordK, pipe, ordV, fn, red, _)), rec) =>
           val op = rec(pipe)
-          op.sortedMapGroup(fn)(ordK, ordV)
-        case (ReduceStepPipe(IteratorMappedReduce(ordK, pipe, fn, _, _)), rec) =>
+          val pc = ConfigPartitionComputer(config, red)
+          op.sortedMapGroup(pc)(fn)(ordK, ordV)
+        case (ReduceStepPipe(IteratorMappedReduce(ordK, pipe, fn, red, _)), rec) =>
           val op = rec(pipe)
-          op.mapGroup(fn)(ordK)
+          val pc = ConfigPartitionComputer(config, red)
+          op.mapGroup(pc)(fn)(ordK)
       }
     })
 
@@ -252,14 +303,18 @@ object SparkPlanner {
           val eleft: Op[(A, Either[B, C])] = planSide(p.larger).map { case (k, v) => (k, Left(v)) }
           val eright: Op[(A, Either[B, C])] = planSide(p.smaller).map { case (k, v) => (k, Right(v)) }
           val joinFn = p.fn
-          (eleft ++ eright).sorted(p.keyOrdering, JoinOrdering()).mapPartitions { it =>
-            val grouped = Iterators.groupSequential(it)
-            grouped.flatMap {
-              case (k, eithers) =>
-                val kfn: Function2[Iterator[B], Iterable[C], Iterator[D]] = joinFn(k, _, _)
-                JoinIterator[B, C, D](kfn)(eithers).map((k, _))
+          val pc = ConfigPartitionComputer(config, p.reducers)
+          // we repartition in sorted, so no need to repartition in merge
+          (eleft.merge(IdentityPartitionComputer, eright))
+            .sorted(pc)(p.keyOrdering, JoinOrdering())
+            .mapPartitions { it =>
+              val grouped = Iterators.groupSequential(it)
+              grouped.flatMap {
+                case (k, eithers) =>
+                  val kfn: Function2[Iterator[B], Iterable[C], Iterator[D]] = joinFn(k, _, _)
+                  JoinIterator[B, C, D](kfn)(eithers).map((k, _))
+              }
             }
-          }
         }
         planPair(pair)
 
