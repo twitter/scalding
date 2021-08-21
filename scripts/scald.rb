efcda56136e08e3bcebf6cb8dfac74378641cab1
Timur Abishev <abishev.timur@gmail.com>
2021-08-21 14:03:21 -0400

    fix with sbt home

--- scripts/scald.rb
+++ scripts/scald.rb
@@ -157,7 +157,7 @@ end
 def find_dependencies(org, dep, version, scala_version=SCALA_VERSION)
   # We need to set scalaVersion because otherwise resolving of scala artifacts fails if version of artifact differs from scalaVersion.
   print("pwd is " + %x[pwd] + "\n")
-  res = %x[./sbt '++#{scala_version}' 'set libraryDependencies := Seq("#{org}" % "#{dep}" % "#{version}")' 'printDependencyClasspath'].split("\n")
+  res = %x[#{SBT_HOME} '++#{scala_version}' 'set libraryDependencies := Seq("#{org}" % "#{dep}" % "#{version}")' 'printDependencyClasspath'].split("\n")
   mapVer = {}
   res.map { |l|
     l,m,r = l.partition(" => ")
