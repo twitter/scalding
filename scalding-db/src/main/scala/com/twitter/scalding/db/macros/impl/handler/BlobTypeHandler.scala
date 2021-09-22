1bfc5c6fa2ae7f649a304ae16eeef781ce705491
tlazaro <tlazaro18@gmail.com>
2021-09-22 09:16:49 -0700

    Update error in BlobTypeHandler

--- scalding-db/src/main/scala/com/twitter/scalding/db/macros/impl/handler/BlobTypeHandler.scala
+++ scalding-db/src/main/scala/com/twitter/scalding/db/macros/impl/handler/BlobTypeHandler.scala
@@ -2,19 +2,20 @@ package com.twitter.scalding.db.macros.impl.handler
 
 import com.twitter.scalding.db.macros.impl.FieldName
 import scala.reflect.macros.Context
-import scala.util.Success
+import scala.util.{ Failure, Success }
 
 object BlobTypeHandler {
-  def apply[T](c: Context)(implicit
+  def apply[T](c: Context)(
+    implicit
     accessorTree: List[c.universe.MethodSymbol],
     fieldName: FieldName,
     defaultValue: Option[c.Expr[String]],
     annotationInfo: List[(c.universe.Type, Option[Int])],
     nullable: Boolean): scala.util.Try[List[ColumnFormat[c.type]]] = {
 
-    require(defaultValue.isEmpty, "Default values are not supported for blob fields")
-    require(annotationInfo.isEmpty, "Annotation info is not supported for blob fields")
-
-    Success(List(ColumnFormat(c)(accessorTree, "BLOB", None)))
+    if (defaultValue.nonEmpty || annotationInfo.nonEmpty)
+      Failure(new Exception(s"Default values and annotation info are not supported: defaultValue = $defaultValue annotationInfo = $annotationInfo"))
+    else
+      Success(List(ColumnFormat(c)(accessorTree, "BLOB", None)))
   }
 }
