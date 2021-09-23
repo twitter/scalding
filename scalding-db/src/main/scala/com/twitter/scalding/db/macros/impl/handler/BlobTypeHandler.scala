a9500fd154fa3a318e2fad063e6d3d15e70e8b17
Tomás Lázaro <tlazaro18@gmail.com>
2021-09-23 14:18:14 -0700

    Adds support for blob, tinyint and timestamp to TypedJDBCSource (#1948)
    * Adds support for blob, tinyint and timestamp to TypedJDBCSource
    
    * Updates in unit tests
    
    * Add require instead of assert
    
    * Update error in BlobTypeHandler

---
+++ scalding-db/src/main/scala/com/twitter/scalding/db/macros/impl/handler/BlobTypeHandler.scala
@@ -0,0 +1,21 @@
+package com.twitter.scalding.db.macros.impl.handler
+
+import com.twitter.scalding.db.macros.impl.FieldName
+import scala.reflect.macros.Context
+import scala.util.{ Failure, Success }
+
+object BlobTypeHandler {
+  def apply[T](c: Context)(
+    implicit
+    accessorTree: List[c.universe.MethodSymbol],
+    fieldName: FieldName,
+    defaultValue: Option[c.Expr[String]],
+    annotationInfo: List[(c.universe.Type, Option[Int])],
+    nullable: Boolean): scala.util.Try[List[ColumnFormat[c.type]]] = {
+
+    if (defaultValue.nonEmpty || annotationInfo.nonEmpty)
+      Failure(new Exception(s"Default values and annotation info are not supported: defaultValue = $defaultValue annotationInfo = $annotationInfo"))
+    else
+      Success(List(ColumnFormat(c)(accessorTree, "BLOB", None)))
+  }
+}
