1312b22b389b6ae226be6e7ab9a3af44bac2fde5
nownikhil <ngoyal@twitter.com>
2022-03-06 11:59:59 -0800

    ToIterable execution in Beam runner (#1962)
    * ToIterable execution in Beam runner
    
    We use the coder from the PCollection and store data as bytes in temp location using FileIO.
    We later read this file using the same coder and get the data on the submitter node.
    TESTS: Added unit tests + Tested with Dataflow Runner
    
    * Fixes
    
    Refactored BeamSink to not have Pipeline object
    
    * Fix
    
    Close input stream when reading output of toIterableExecution
    
    * Cleanup test

--- scalding-beam/src/main/scala/com/twitter/scalding/beam_backend/BeamMode.scala
+++ scalding-beam/src/main/scala/com/twitter/scalding/beam_backend/BeamMode.scala
@@ -3,8 +3,11 @@ package com.twitter.scalding.beam_backend
 import com.twitter.scalding.Execution.Writer
 import com.twitter.scalding.typed.{Resolver, TypedSink, TypedSource}
 import com.twitter.scalding.{Config, Mode, TextLine}
+import java.io.{EOFException, InputStream}
+import java.nio.channels.{Channels, WritableByteChannel}
 import org.apache.beam.sdk.Pipeline
-import org.apache.beam.sdk.io.TextIO
+import org.apache.beam.sdk.coders.Coder
+import org.apache.beam.sdk.io.{FileIO, TextIO}
 import org.apache.beam.sdk.options.PipelineOptions
 import org.apache.beam.sdk.values.PCollection
 
@@ -50,7 +53,7 @@ object BeamSource extends Serializable {
 }
 
 trait BeamSink[-A] extends Serializable {
-  def write(pipeline: Pipeline, config: Config, pc: PCollection[_ <: A]): Unit
+  def write(pc: PCollection[_ <: A], config: Config): Unit
 }
 
 object BeamSink extends Serializable {
@@ -70,7 +73,58 @@ object BeamSink extends Serializable {
 
   def textLine(path: String): BeamSink[String] =
     new BeamSink[String] {
-      override def write(pipeline: Pipeline, config: Config, pc: PCollection[_ <: String]): Unit =
-        pc.asInstanceOf[PCollection[String]].apply(TextIO.write().to(path))
+      override def write(pc: PCollection[_ <: String], config: Config): Unit = {
+        val stringPCollection: PCollection[String] = BeamFunctions.widenPCollection(pc)
+        stringPCollection.apply(TextIO.write().to(path))
+      }
+    }
+}
+
+class BeamFileIO[T](output: String) extends BeamSink[T] {
+  override def write(
+      pc: PCollection[_ <: T],
+      config: Config
+  ): Unit = {
+    val pColT: PCollection[T] = BeamFunctions.widenPCollection(pc)
+
+    pColT.apply(
+      FileIO
+        .write()
+        .via(new CoderFileSink(pColT.getCoder))
+        .to(output)
+    )
+  }
+}
+
+class CoderFileSink[T](coder: Coder[T]) extends FileIO.Sink[T] {
+  private var outputStream: java.io.OutputStream = _
+
+  override def open(channel: WritableByteChannel): Unit =
+    outputStream = Channels.newOutputStream(channel)
+
+  override def write(element: T): Unit = coder.encode(element, outputStream)
+  override def flush(): Unit = outputStream.flush()
+}
+
+class InputStreamIterator[T](stream: InputStream, coder: Coder[T]) extends Iterator[T] {
+  var hasNextRecord: Boolean = _
+  var nextRecord: T = _
+
+  fetchNext()
+  override def hasNext: Boolean = hasNextRecord
+
+  override def next(): T = {
+    val recordToReturn = nextRecord
+    fetchNext()
+    recordToReturn
+  }
+
+  private def fetchNext(): Unit =
+    try {
+      nextRecord = coder.decode(stream)
+      hasNextRecord = true
+    } catch {
+      case _: EOFException =>
+        hasNextRecord = false
     }
 }
--- scalding-beam/src/main/scala/com/twitter/scalding/beam_backend/BeamWriter.scala
+++ scalding-beam/src/main/scala/com/twitter/scalding/beam_backend/BeamWriter.scala
@@ -4,13 +4,23 @@ import com.stripe.dagon.Rule
 import com.twitter.scalding.Execution.{ToWrite, Writer}
 import com.twitter.scalding.typed._
 import com.twitter.scalding.{CFuture, CancellationHandler, Config, Execution, ExecutionCounters}
+import java.io.InputStream
+import java.nio.channels.Channels
 import java.util.concurrent.atomic.AtomicLong
+import java.util.concurrent.ConcurrentHashMap
 import org.apache.beam.sdk.Pipeline
+import org.apache.beam.sdk.coders.Coder
+import org.apache.beam.sdk.io.FileSystems
 import scala.annotation.tailrec
+import scala.collection.convert.decorateAsScala._
 import scala.concurrent.{ExecutionContext, Future}
 
 class BeamWriter(val beamMode: BeamMode) extends Writer {
   private val state = new AtomicLong()
+
+  private val sourceCounter: AtomicLong = new AtomicLong(0L)
+  private val iterablePipes: scala.collection.concurrent.Map[TypedPipe[_], (Coder[_], String)] =
+    new ConcurrentHashMap[TypedPipe[_], (Coder[_], String)]().asScala
 
   override def start(): Unit = ()
 
@@ -22,7 +32,33 @@ class BeamWriter(val beamMode: BeamMode) extends Writer {
 
   def getIterable[T](conf: Config, initial: TypedPipe[T])(implicit
       cec: ExecutionContext
-  ): Future[Iterable[T]] = ???
+  ): Future[Iterable[T]] =
+    iterablePipes.get(initial) match {
+      case Some((coder, path)) =>
+        val c: Coder[T] = coder.asInstanceOf[Coder[T]]
+        Future(new Iterable[T] {
+          // Single dir by default just matches the dir, we need to match files inside
+          val matchedResources = FileSystems.`match`(s"$path*").metadata().asScala
+
+          override def iterator: Iterator[T] = {
+            // an empty Iterator that closes an InputStream when it is iterated
+            def closeIt(is: InputStream): Iterator[T] =
+              new Iterator[T] {
+                def hasNext: Boolean = {
+                  is.close()
+                  false
+                }
+                def next = Iterator.empty.next
+              }
+
+            matchedResources.iterator
+              .flatMap { resource =>
+                val is = Channels.newInputStream(FileSystems.open(resource.resourceId()))
+                new InputStreamIterator(is, c) ++ closeIt(is)
+              }
+          }
+        })
+    }
 
   override def execute(conf: Config, writes: List[ToWrite[_]])(implicit
       cec: ExecutionContext
@@ -43,12 +79,23 @@ class BeamWriter(val beamMode: BeamMode) extends Writer {
               val pcoll = planner(opt).run(pipeline)
               beamMode.sink(sink) match {
                 case Some(ssink) =>
-                  ssink.write(pipeline, conf, pcoll)
+                  ssink.write(pcoll, conf)
                 case _ => throw new Exception(s"unknown sink: $sink when writing $pipe")
               }
               rec(xs)
             }
-            //TODO: handle Force and ToIterable
+            case OptimizedWrite(pipe, ToWrite.ToIterable(opt)) =>
+              val pcoll = planner(opt).run(pipeline)
+              val tempLocation = pcoll.getPipeline.getOptions.getTempLocation
+              require(tempLocation != null, "Temp location cannot be null when using toIterableExecution")
+
+              val outputPath = BeamWriter.addPaths(tempLocation, sourceCounter.getAndIncrement().toString)
+              // Here we add a sink transformation on the PCollection.
+              // This does not run till the final `pipeline.run` step
+              new BeamFileIO(outputPath).write(pcoll, conf)
+              iterablePipes += ((pipe, (pcoll.getCoder, outputPath)))
+
+            //TODO: handle Force
             case _ => ???
           }
       }
@@ -65,4 +112,11 @@ class BeamWriter(val beamMode: BeamMode) extends Writer {
       }
     )
   }
+}
+
+object BeamWriter {
+  // This is manually done because java.nio.File.Paths & java.io.File convert "gs://" to "gs:/"
+  def addPaths(basePath: String, dir: String): String =
+    if (basePath.endsWith("/")) s"$basePath$dir/"
+    else s"$basePath/$dir/"
 }
