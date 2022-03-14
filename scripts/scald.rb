b70422d0cfccf89b0d6771492659de6840cb4ad9
Daniel Sudzilouski <daniel@sudzilouski.com>
2022-03-14 15:45:53 -0700

    fix scalding version in scald.rb

--- scripts/scald.rb
+++ scripts/scald.rb
@@ -58,7 +58,7 @@ CONFIG_RC = begin
 CONFIG = CONFIG_DEFAULT.merge!(CONFIG_RC)
 
 BUILDFILE = open(CONFIG["repo_root"] + "/build.sbt").read
-SCALDING_VERSION=`./sbt -Dsbt.supershell=false "print scalding-core / version" -error`
+SCALDING_VERSION=`#{CONFIG["repo_root"] + "/sbt"} -Dsbt.supershell=false "print scalding-core / version" -error`.strip
 
 #optionally set variables (not linux often doesn't have this set, and falls back to TMP. Set up a
 #YAML file in .scaldrc with "tmpdir: my_tmp_directory_name" or export TMPDIR="/my/tmp" to set on
